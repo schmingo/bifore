e8047d729886f695c3ac893ff69ba50e7d1afc58
schmingo <sschlauss@gmail.com>
2013-07-25 11:34:27 +0200

    initialize plotting file

---
+++ plotting/plot_extract_vals_landsat8.R
@@ -0,0 +1,9 @@
+################################################################################
+## BiFoRe Scripts
+##
+## PLOTTING EXTRACTED VALUES FROM SATELLITE DATA
+##
+## Author: Simon Schlauss (sschlauss@gmail.com)
+## Version: 2013-07-25
+##
+################################################################################

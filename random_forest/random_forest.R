f5705fecf284defc913330e62ef37324b7666309
schmingo <sschlauss@gmail.com>
2013-07-25 11:52:51 +0200

    basic setup (load library, setwd)

--- random_forest/random_forest.R
+++ random_forest/random_forest.R
@@ -7,3 +7,16 @@
 ## Version: 2013-07-25
 ##
 ################################################################################
+
+## Clear workspace
+rm(list = ls(all = TRUE))
+
+## Required libraries
+lib <- c("randomForest")
+lapply(lib, function(...) require(..., character.only = TRUE))
+
+## set working directory
+setwd("/home/schmingo/Diplomarbeit/") # Linux
+setwd("D:/Diplomarbeit/") # Windows
+setwd("hier_kommt_der_Flo ;-)") # Linux
+setwd("hier_kommt_der_Flo ;-)") # Windows

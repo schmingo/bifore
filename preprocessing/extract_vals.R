6db6fca804f64bdcf6216fb5c7dc294b65250c9e
schmingo <sschlauss@gmail.com>
2013-07-25 12:50:48 +0200

    formal edits also deleted unused stuff

--- preprocessing/extract_vals.R
+++ preprocessing/extract_vals.R
@@ -22,7 +22,7 @@ setwd("hier_kommt_der_Flo ;-)") # Linux
 setwd("hier_kommt_der_Flo ;-)") # Windows
 
 
-### Landsat data
+### Import Landsat data
 
 ## List files
 files.list.sat <- list.files("src/satellite/Landsat8_2013-07-07_hai/Level1_GeoTIFF_Data_Product/", 
@@ -37,39 +37,42 @@ raster.layers <- lapply(files.list.sat, raster)
 projection.layers <- CRS(projection(raster.layers[[1]]))
 
 
-### Station data
+### Create extends from *.csv
 
-## List center files
-files.hai.center <- list.files("src/csv/", pattern = "hai_plot_center.csv$", full.names = TRUE)
+# ## Option 1: List CENTER files
+# files.hai.center <- list.files("src/csv/", pattern = "hai_plot_center.csv$", full.names = TRUE)
+# 
+# ## Import CENTER files as SpatialPointsDataframe objects
+# table.hai.center <- read.csv2(files.hai.center, dec = ".", stringsAsFactors = FALSE)
+# coordinates(table.hai.center) <- c("Longitude", "Latitude")
+# projection(table.hai.center) <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+# 
+# table.hai.center <- spTransform(table.hai.center, CRS = projection.layers)
 
-## Import center files as SpatialPointsDataframe objects
-table.hai.center <- read.csv2(files.hai.center, dec = ".", stringsAsFactors = FALSE)
-coordinates(table.hai.center) <- c("Longitude", "Latitude")
-projection(table.hai.center) <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
-
-table.hai.center <- spTransform(table.hai.center, CRS = projection.layers)
-
-## List corner files
+## Option 2: List CORNER files
 files.hai.corner <- list.files("src/csv/", pattern = "hai_corner.csv$", full.names = TRUE)
 
-## Import corner files as SpatialPointsDataframe objects
+## Import CORNER files as SpatialPointsDataframe objects
 table.hai <- read.csv2(files.hai.corner, dec = ".", stringsAsFactors = FALSE)
 coordinates(table.hai) <- c("Longitude", "Latitude")
 projection(table.hai) <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
   
 table.hai <- spTransform(table.hai, CRS = projection.layers)
 
-## Retrieve extent from corner coordinates
+## Retrieve extent from CORNER coordinates
 extent.hai <- lapply(seq(1, nrow(table.hai), 4), function(i) {
   extent(coordinates(table.hai[i:(i+3), ]))
-})
+  })
+
+
+### Extraction
 
 ## Parallelization
 clstr <- makePSOCKcluster(n.cores <- 4)
 clusterExport(clstr, c("lib", "raster.layers", "extent.hai", "table.hai.center", "table.hai"))
 clusterEvalQ(clstr, lapply(lib, function(i) require(i, character.only = TRUE, quietly = TRUE)))
 
-## Extract and average cell values
+## Extract and AVERAGE cell values
 values.hai <- parLapply(clstr, raster.layers, function(h) {
   temp.values <- sapply(extent.hai, function(i) {
     temp.extract <- extract(h, i)
@@ -92,11 +95,6 @@ coordinates(values.hai.all) <- c("Longitude", "Latitude")
 
 ## Deregister parallel backend
 stopCluster(clstr)
-
-# show data
-str(values.hai.all)
-show(values.hai.all)
-show(values.hai.all$B1)
 
 ## Write data to new csv
 write.table(values.hai.all, file = "src/csv/hai_greyvalues_landsat8.csv", dec = ".", quote = FALSE, 

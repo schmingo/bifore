72eb83b113ace20bcb9e7aec3760d00626640f30
schmingo <sschlauss@gmail.com>
2013-07-26 10:21:30 +0200

    create subsets

--- plotting/plot_extract_vals_landsat8.R
+++ plotting/plot_extract_vals_landsat8.R
@@ -22,10 +22,22 @@ setwd("hier_kommt_der_Flo ;-)") # Linux
 setwd("hier_kommt_der_Flo ;-)") # Windows
 
 ## Import files
-table.hai <- read.csv2("src/csv/hai_greyvalues_landsat8.csv", dec = ".", stringsAsFactors = FALSE)
+table.hai.all <- read.csv2("src/csv/hai_greyvalues_landsat8.csv", dec = ".", stringsAsFactors = FALSE))
 
 
-## Plot greyvalues of B1 for all Hainich plots
-scatter.ggplot <- ggplot(aes(x = Plotname, y = B1), data = table.hai)
+## Create Subsets
+table.hai.grass <- subset(table.hai.all, Location == "Grassland")
+table.hai.forest <- subset(table.hai.all, Location == "Forest")
+
+table.hai.grass <- table.hai.grass[, -c(2:6)] # deletes unused columns
+table.hai.forest <- table.hai.forest[, -c(2:6)]
+
+#df <- df[, c(1, 5:10)] # fügt bestimmt spalten zusammen
+#df <- df[, -c(2:4)] # löscht bestimmte spalten
+
+
+
+## Plot greyvalues of a specific channel
+scatter.ggplot <- ggplot(aes(x = Plotname, y = B1), data = table.hai.grass)
 g.sc <- scatter.ggplot + geom_point()
 print(g.sc)

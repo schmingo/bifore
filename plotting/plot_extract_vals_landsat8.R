ef198c32c4a547f1f3ccaa0a935068788cc6b7ba
schmingo <sschlauss@gmail.com>
2013-07-26 09:47:39 +0200

    first plotting with ggplot2 all greyvalues in B1 in every hai plot to visualize outliers

--- plotting/plot_extract_vals_landsat8.R
+++ plotting/plot_extract_vals_landsat8.R
@@ -21,7 +21,11 @@ setwd("D:/Diplomarbeit/") # Windows
 setwd("hier_kommt_der_Flo ;-)") # Linux
 setwd("hier_kommt_der_Flo ;-)") # Windows
 
-
 ## Import files
-table.hai.land8 <- "src/csv/hai_greyvalues_landsat8.csv$"
+table.hai <- read.csv2("src/csv/hai_greyvalues_landsat8.csv", dec = ".", stringsAsFactors = FALSE)
 
+
+## Plot greyvalues of B1 for all Hainich plots
+scatter.ggplot <- ggplot(aes(x = Plotname, y = B1), data = table.hai)
+g.sc <- scatter.ggplot + geom_point()
+print(g.sc)

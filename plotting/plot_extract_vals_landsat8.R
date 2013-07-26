7fec04ef2624f57879eb30a0625b87a417d360c4
schmingo <sschlauss@gmail.com>
2013-07-26 13:14:53 +0200

    change version

--- plotting/plot_extract_vals_landsat8.R
+++ plotting/plot_extract_vals_landsat8.R
@@ -4,7 +4,7 @@
 ## PLOTTING EXTRACTED VALUES FROM SATELLITE DATA
 ##
 ## Author: Simon Schlauss (sschlauss@gmail.com)
-## Version: 2013-07-25
+## Version: 2013-07-26
 ##
 ################################################################################
 
@@ -16,10 +16,10 @@ lib <- c("ggplot2")
 lapply(lib, function(...) require(..., character.only = TRUE))
 
 ## Set working directory
-#setwd("/home/schmingo/Diplomarbeit/") # Linux
+setwd("/home/schmingo/Diplomarbeit/") # Linux
 setwd("D:/Diplomarbeit/") # Windows
-#setwd("hier_kommt_der_Flo ;-)") # Linux
-#setwd("hier_kommt_der_Flo ;-)") # Windows
+setwd("hier_kommt_der_Flo ;-)") # Linux
+setwd("hier_kommt_der_Flo ;-)") # Windows
 
 ## Import files
 table.all <- read.csv2("src/csv/hai_greyvalues_landsat8.csv", dec = ".", stringsAsFactors = FALSE)

8e0ce881e7939fcd9a16ad29771b30c0fff8c228
schmingo <sschlauss@gmail.com>
2013-07-26 10:59:11 +0200

    Generalization

--- plotting/plot_extract_vals_landsat8.R
+++ plotting/plot_extract_vals_landsat8.R
@@ -22,15 +22,15 @@ setwd("hier_kommt_der_Flo ;-)") # Linux
 setwd("hier_kommt_der_Flo ;-)") # Windows
 
 ## Import files
-table.hai.all <- read.csv2("src/csv/hai_greyvalues_landsat8.csv", dec = ".", stringsAsFactors = FALSE))
+table.all <- read.csv2("src/csv/hai_greyvalues_landsat8.csv", dec = ".", stringsAsFactors = FALSE))
 
 
 ## Create Subsets
-table.hai.grass <- subset(table.hai.all, Location == "Grassland")
-table.hai.forest <- subset(table.hai.all, Location == "Forest")
+table.grass <- subset(table.all, Location == "Grassland")
+table.hai.forest <- subset(table.all, Location == "Forest")
 
-table.hai.grass <- table.hai.grass[, -c(2:6)] # deletes unused columns
-table.hai.forest <- table.hai.forest[, -c(2:6)]
+table.grass <- table.grass[, -c(2:6)] # deletes unused columns
+table.forest <- table.forest[, -c(2:6)]
 
 #df <- df[, c(1, 5:10)] # fügt bestimmt spalten zusammen
 #df <- df[, -c(2:4)] # löscht bestimmte spalten
@@ -38,6 +38,6 @@ table.hai.forest <- table.hai.forest[, -c(2:6)]
 
 
 ## Plot greyvalues of a specific channel
-scatter.ggplot <- ggplot(aes(x = Plotname, y = B1), data = table.hai.grass)
+scatter.ggplot <- ggplot(aes(x = Plotname, y = B1), data = table.grass)
 g.sc <- scatter.ggplot + geom_point()
 print(g.sc)

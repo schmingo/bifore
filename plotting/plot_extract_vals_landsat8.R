eaca40044dced7b7ab01b5914d1d2bb30afdb45f
Florian Detsch <florian.detsch@staff.uni-marburg.de>
2013-07-27 11:29:43 +0200

    Update: reformat colnames / xyplot approach

--- plotting/plot_extract_vals_landsat8.R
+++ plotting/plot_extract_vals_landsat8.R
@@ -12,17 +12,19 @@
 rm(list = ls(all = TRUE))
 
 ## Required libraries
-lib <- c("ggplot2")
+lib <- c("ggplot2", "latticeExtra")
 lapply(lib, function(...) require(..., character.only = TRUE))
 
 ## Set working directory
 setwd("/home/schmingo/Diplomarbeit/") # Linux
 setwd("D:/Diplomarbeit/") # Windows
-setwd("hier_kommt_der_Flo ;-)") # Linux
-setwd("hier_kommt_der_Flo ;-)") # Windows
+# setwd("hier_kommt_der_Flo ;-)") # Linux
+setwd("E:/repositories/scripts") # Windows
 
 ## Import files
 table.all <- read.csv2("src/csv/hai_greyvalues_landsat8.csv", dec = ".", stringsAsFactors = FALSE)
+table.all <- read.csv2("C:/Users/fdetsch/Downloads/hai_greyvalues_landsat8.csv", 
+                       dec = ".", stringsAsFactors = FALSE)
 
 
 ## Create Subsets
@@ -35,6 +37,13 @@ table.forest <- table.forest[, -c(2:6)]
 #df <- df[, c(1, 5:10)] # fügt bestimmt spalten zusammen
 #df <- df[, -c(2:4)] # löscht bestimmte spalten
 
+# Load and execute function to reformat colnames of input data
+source("plotting/reformatColnames.R")
+
+table.grass <- reformatColnames(table.grass)
+table.forest <- reformatColnames(table.forest)
+
+
 ## Plot greyvalues of a specific channel
 scatter.ggplot <- ggplot(aes(x = Plotname, y = B1), data = table.grass)
 g.sc <- scatter.ggplot + geom_point()
@@ -42,3 +51,9 @@ print(g.sc)
 
 ## Plot using qplot
 qplot(Plotname, B1, data = table.grass, geom = "point")
+
+## Plot using xyplot from 'latticeExtra' package
+table.grass.heg01 <- subset(table.grass, Plotname == "HEG01")
+xyplot(table.grass.heg01[, 2:ncol(table.grass.heg01)] ~ 
+         as.factor(names(table.grass.heg01[2:ncol(table.grass.heg01)])), 
+       type = "b", xlab = "Bands", ylab = "Grey value")
---
+++ plotting/reformatColnames.R
@@ -0,0 +1,15 @@
+# Function to reformat colnames of input data
+reformatColnames <- function(x) {
+  # Edit band names
+  tmp.names <- names(x)[2:(ncol(x)-1)]
+  tmp.bands <- as.numeric(sapply(strsplit(tmp.names, "B"), "[[", 2))
+  tmp.bands <- formatC(tmp.bands, width = 2, format = "d", flag = "0")
+  
+  names(x)[2:(ncol(i)-1)] <- paste("B", tmp.bands, sep = "")
+  
+  # Rearrange columns in ascending order
+  x <- x[, c(1, order(names(x)[2:ncol(x)]) + 1)]
+  
+  # Return data.frame
+  return(x)
+}

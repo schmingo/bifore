dfbdde9e61122843d5a48a20119624bb66213cc8
schmingo <sschlauss@gmail.com>
2013-07-28 15:35:19 +0200

    ggplot try

--- plotting/plot_extract_vals_landsat8.R
+++ plotting/plot_extract_vals_landsat8.R
@@ -40,10 +40,21 @@ g.sc <- scatter.ggplot + geom_point()
 print(g.sc)
 
 ## Plot using qplot
-qplot(Plotname, B1, data = table.grass, geom = "point")
+qplot(Plotname, B01, data = table.grass, geom = "point")
 
 ## Plot using xyplot from 'latticeExtra' package
 table.grass.heg01 <- subset(table.grass, Plotname == "HEG01")
 xyplot(table.grass.heg01[, 2:ncol(table.grass.heg01)] ~ 
          as.factor(names(table.grass.heg01[2:ncol(table.grass.heg01)])), 
        type = "b", xlab = "Bands", ylab = "Grey value")
+
+## ggplot try
+str(table.grass.heg01)
+print(colnames(table.grass.heg01))
+print(as.factor(table.grass.heg01[1,2:13]))
+print(table.grass.heg01)
+
+ggplot(data = table.grass.heg01, aes(x = as.factor(names(table.grass.heg01[2:ncol(table.grass.heg01)])), 
+                                     y = table.grass.heg01[1,2:13])) +  geom_point()
+
+qplot(as.factor(names(table.grass.heg01[2:ncol(table.grass.heg01)])), table.grass.heg01[1,2:13], data = table.grass.heg01, geom = "point")

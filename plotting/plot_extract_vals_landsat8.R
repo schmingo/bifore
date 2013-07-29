2eac87f7a0b6d886d85a0441dc0b6c2c2cba86ce
schmingo <sschlauss@gmail.com>
2013-07-29 12:34:50 +0200

    rename variables

--- plotting/plot_extract_vals_landsat8.R
+++ plotting/plot_extract_vals_landsat8.R
@@ -22,50 +22,50 @@ setwd("/home/schmingo/Diplomarbeit/") # Linux
 #setwd("E:/repositories/scripts") # Windows
 
 ## Import data 
-table.all <- read.csv2("src/csv/hai_greyvalues_landsat8.csv", dec = ".",
+all <- read.csv2("src/csv/hai_greyvalues_landsat8.csv", dec = ".",
                        header = TRUE, row.name = 1, stringsAsFactors = FALSE)
 
 
 ### Create Subsets
 
 ## different locations
-table.grass <- subset(table.all, Location == "Grassland")
-table.forest <- subset(table.all, Location == "Forest")
+grass <- subset(all, Location == "Grassland")
+forest <- subset(all, Location == "Forest")
 
 ## delete redundant columns (coordinates, etc.)
-table.grass <- table.grass[, -c(1:5)]
-table.forest <- table.forest[, -c(1:5)]
+grass <- grass[, -c(1:5)]
+forest <- forest[, -c(1:5)]
 
 ## special exploratories-plots
-table.grass.123 <- table.grass[c(1:3),]
+grass.123 <- grass[c(1:3),]
 
 ## transpose dataframes
-table.grass.trans <- data.frame(t(table.grass))
-table.forest.trans <- data.frame(t(table.forest))
-table.grass.123.trans <- data.frame(t(table.grass.123))
+grass.t <- data.frame(t(grass))
+forest.t <- data.frame(t(forest))
+grass.123.t <- data.frame(t(grass.123))
 
 ########################### Plotting ###########################################
 
 
 ### boxplot
 ## melt dataframes for boxplot
-table.grass.trans.melt <- melt(table.grass.trans) # melt dataframes for boxplot
-table.grass.123.trans.melt <- melt(table.grass.123.trans) # melt dataframes for boxplot
+grass.t.melt <- melt(grass.t) # melt dataframes for boxplot
+grass.123.t.melt <- melt(grass.123.t) # melt dataframes for boxplot
 
-ggplot(data = table.grass.123.trans.melt, aes(x = variable, y = value))+ geom_boxplot()
-ggplot(data = table.grass.trans.melt, aes(x = variable, y = value))+ geom_boxplot()
+ggplot(data = grass.123.t.melt, aes(x = variable, y = value))+ geom_boxplot()
+ggplot(data = grass.t.melt, aes(x = variable, y = value))+ geom_boxplot()
 
 
 ### scatterplot
-ggplot(data = table.grass.trans, aes(rownames(table.grass.trans), HEG01)) + geom_point()
+ggplot(data = grass.t, aes(rownames(grass.t), HEG01)) + geom_point()
 
 ## scatterplot (qplot-version)
-qplot(rownames(table.grass.trans),HEG01, data = table.grass.trans, geom=c("point"))
+qplot(rownames(grass.t),HEG01, data = grass.t, geom=c("point"))
 
 ################################################################################
 # ## Plot by Florian
-# table.grass.heg01 <- subset(table.grass[1], Plotname == "HEG01")
+# grass.heg01 <- subset(grass[1], Plotname == "HEG01")
 # 
-# xyplot(table.grass[1,2:ncol(table.grass)] ~ 
-#       as.factor(names(table.grass[2:ncol(table.grass)])), 
+# xyplot(grass[1,2:ncol(grass)] ~ 
+#       as.factor(names(grass[2:ncol(grass)])), 
 #       type = "b", xlab = "Bands", ylab = "Grey value")

6337b61d22712c78b218f9fb1e6b742dac846b6a
schmingo <sschlauss@gmail.com>
2013-07-29 10:10:46 +0200

    transpose df should make it easier for ggplot now :)

--- plotting/plot_extract_vals_landsat8.R
+++ plotting/plot_extract_vals_landsat8.R
@@ -16,48 +16,65 @@ lib <- c("ggplot2", "latticeExtra")
 lapply(lib, function(...) require(..., character.only = TRUE))
 
 ## Set working directory
-#setwd("/home/schmingo/Diplomarbeit/") # Linux
-setwd("D:/Diplomarbeit/") # Windows
+setwd("/home/schmingo/Diplomarbeit/") # Linux
+#setwd("D:/Diplomarbeit/") # Windows
 # setwd("hier_kommt_der_Flo ;-)") # Linux
 #setwd("E:/repositories/scripts") # Windows
 
-## Import files
-table.all <- read.csv2("src/csv/hai_greyvalues_landsat8.csv", dec = ".", stringsAsFactors = FALSE)
+## Import data 
+table.all <- read.csv2("src/csv/hai_greyvalues_landsat8.csv", dec = ".",
+                       header = TRUE, row.names = 1, stringsAsFactors = FALSE)
 #table.all <- read.csv2("C:/Users/fdetsch/Downloads/hai_greyvalues_landsat8.csv", dec = ".", stringsAsFactors = FALSE)
+str(table.all)
 
+## Transpose data
+table.all.trans <- data.frame(t(table.all))
+str(table.all.trans)
 
 ## Create Subsets
 table.grass <- subset(table.all, Location == "Grassland")
 table.forest <- subset(table.all, Location == "Forest")
 
-table.grass <- table.grass[, -c(2:6)] # deletes unused columns
-table.forest <- table.forest[, -c(2:6)]
+table.grass <- table.grass[, -c(1:5)] # deletes unused columns
+table.forest <- table.forest[, -c(1:5)]
 
+table.grass.trans <- data.frame(t(table.grass))
+table.forest.trans <- data.frame(t(table.forest))
+
+
+################################################################################
+######################### Plotting #############################################
+################################################################################
 ## Plot greyvalues of a specific channel
-scatter.ggplot <- ggplot(aes(x = Plotname, y = B1), data = table.grass)
-g.sc <- scatter.ggplot + geom_point()
-print(g.sc)
+# scatter.ggplot <- ggplot(aes(x = Plotname, y = B1), data = table.grass)
+# g.sc <- scatter.ggplot + geom_point()
+# print(g.sc)
 
 ## Plot using qplot
-qplot(Plotname, B01, data = table.grass, geom = "point")
+# qplot(Plotname, B01, data = table.grass, geom = "point")
 
 ## Plot using xyplot from 'latticeExtra' package
-table.grass.heg01 <- subset(table.grass, Plotname == "HEG01")
+table.grass.heg01 <- subset(table.grass[1] Plotname == "HEG01")
 xyplot(table.grass.heg01[, 2:ncol(table.grass.heg01)] ~ 
-         as.factor(names(table.grass.heg01[2:ncol(table.grass.heg01)])), 
-       type = "b", xlab = "Bands", ylab = "Grey value")
+      as.factor(names(table.grass.heg01[2:ncol(table.grass.heg01)])), 
+      type = "b", xlab = "Bands", ylab = "Grey value")
 
 ## ggplot try
-str(table.grass.heg01)
-print(colnames(table.grass.heg01))
-print(table.grass.heg01[1,2:13])
-print(table.grass.heg01)
-print(table.grass)
-summary(table.grass.heg01)
-str(table.grass.heg01)
-show(table.grass.heg01)
+################################################################################
 
+str(table.grass)
+
+ggplot(table.grass, aes(colnames(table.grass),table.grass[1])) + geom_bar()
+
+data(diamonds)
+str(diamonds)
+
+
+
+################################################################################
 hefeweizen <- ggplot(data = table.grass, aes(x = as.factor(names(table.grass[2:ncol(table.grass)])), 
-                                     y = table.grass[1,2:ncol(table.grass)])  +  geom_point()
+                                             y = table.grass[1,2:ncol(table.grass)]) +  geom_point()
                      
-qplot(as.factor(names(table.grass.heg01[2:ncol(table.grass.heg01)])), data.frame(table.grass.heg01[1,2:13]), data = table.grass.heg01, geom = "bar")
+qplot(as.factor(colnames(table.grass.heg01[2:ncol(table.grass.heg01)])), 
+      table.grass.heg01[1,2:ncol(table.grass.heg01)], data = table.grass.heg01, 
+      geom = "bar")

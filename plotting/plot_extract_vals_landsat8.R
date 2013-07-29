b7ac0e31a8c9fcd69555d0f270627d8e1683d0bc
schmingo <sschlauss@gmail.com>
2013-07-29 10:38:41 +0200

    ggplot try

--- plotting/plot_extract_vals_landsat8.R
+++ plotting/plot_extract_vals_landsat8.R
@@ -41,40 +41,23 @@ table.forest <- table.forest[, -c(2:6)]
 table.grass.trans <- data.frame(t(table.grass))
 table.forest.trans <- data.frame(t(table.forest))
 
+str(table.grass.trans)
+summary(table.grass.trans)
+print(row.names(table.grass.trans))
 
 ################################################################################
 ######################### Plotting #############################################
 ################################################################################
-## Plot greyvalues of a specific channel
-# scatter.ggplot <- ggplot(aes(x = Plotname, y = B1), data = table.grass)
-# g.sc <- scatter.ggplot + geom_point()
-# print(g.sc)
 
-## Plot using qplot
-# qplot(Plotname, B01, data = table.grass, geom = "point")
+## ggplot try
+ggplot(table.grass.trans, aes(rownames(table.grass.trans),X1)) + geom_point()
+
+
+
 
 ## Plot using xyplot from 'latticeExtra' package
 table.grass.heg01 <- subset(table.grass[1] Plotname == "HEG01")
+
 xyplot(table.grass.heg01[, 2:ncol(table.grass.heg01)] ~ 
       as.factor(names(table.grass.heg01[2:ncol(table.grass.heg01)])), 
       type = "b", xlab = "Bands", ylab = "Grey value")
-
-## ggplot try
-################################################################################
-
-str(table.grass)
-
-ggplot(table.grass, aes(colnames(table.grass),table.grass[1])) + geom_bar()
-
-data(diamonds)
-str(diamonds)
-
-
-
-################################################################################
-hefeweizen <- ggplot(data = table.grass, aes(x = as.factor(names(table.grass[2:ncol(table.grass)])), 
-                                             y = table.grass[1,2:ncol(table.grass)]) +  geom_point()
-                     
-qplot(as.factor(colnames(table.grass.heg01[2:ncol(table.grass.heg01)])), 
-      table.grass.heg01[1,2:ncol(table.grass.heg01)], data = table.grass.heg01, 
-      geom = "bar")

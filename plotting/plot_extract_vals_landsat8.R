f84b4ba70855779953a9e865e016068bcbf76645
schmingo <sschlauss@gmail.com>
2013-07-31 13:30:20 +0200

    add new lineplots (meeting flo)

--- plotting/plot_extract_vals_landsat8.R
+++ plotting/plot_extract_vals_landsat8.R
@@ -57,12 +57,31 @@ all.melt <- melt(all,
                  id = c("Plotname", "Plotid","Status", "Location", "Longitude", "Latitude"),
                  measured = c(all[,7:nrow(all)]))
 
+
 ## plot using ggplot2 package
 ggplot(data = grass.melt, aes(x = variable, y = value))+ geom_boxplot()
 ggplot(data = forest.melt, aes(x = variable, y = value))+ geom_boxplot()
 ggplot(data = grass.123.melt, aes(x = variable, y = value, colour=Plotname)) + geom_point()
 
 ggplot(data = all.melt, aes(x = variable, y = value, colour=Location)) + geom_boxplot()
+
+
+### lineplot
+tmp <- melt(all[, c(1, 7:ncol(all))], id.vars = "Plotname", variable.name = "Band")
+tmp <- tmp[order(tmp$Plotname), ]
+ggplot(data = tmp, aes(x = Band, y = value, group = Plotname)) + 
+  geom_line(aes(colour = Plotname))
+
+
+### subset lineplot with facets
+tmp2 <- subset(tmp, Plotname %in% c("HEG01", "HEG02", "HEG03", "HEG04", "HEG05"))
+ggplot(data = tmp2, aes(x = Band, y = value, group = Plotname)) + 
+  #   geom_point() + 
+  geom_line(aes(colour = Plotname)) + 
+  facet_wrap(~ Plotname, nrow = 2, ncol = 3)
+
+
+cbind(tmp2, sequence = seq(nrow(tmp2))) # generate column with seqence of nrow of tmp2
 
 ################################################################################
 # ## Plot by Florian

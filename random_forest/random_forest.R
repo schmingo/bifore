9a10a27ec1948e4cef438c7f0fa1416919a2370c
schmingo <sschlauss@gmail.com>
2013-08-13 09:09:25 +0200

    remove default randomForest function

--- random_forest/random_forest.R
+++ random_forest/random_forest.R
@@ -21,22 +21,3 @@ setwd("D:/Diplomarbeit/") # Windows
 #setwd("Florian")
 
 ## Import dataset
-data <- read.csv2("src/csv/all_greyvalues_modis_NA_abundance.csv", dec = ".",
-                  header = TRUE, stringsAsFactors = FALSE)
-
-??randomForest
-
-randomForest(formula, data=NULL, ..., subset, na.action=na.fail)
-
-randomForest(x, y=NULL,  xtest=NULL, ytest=NULL, ntree=500,
-             mtry=if (!is.null(y) && !is.factor(y))
-               max(floor(ncol(x)/3), 1) else floor(sqrt(ncol(x))),
-             replace=TRUE, classwt=NULL, cutoff, strata,
-             sampsize = if (replace) nrow(x) else ceiling(.632*nrow(x)),
-             nodesize = if (!is.null(y) && !is.factor(y)) 5 else 1,
-             maxnodes = NULL,
-             importance=FALSE, localImp=FALSE, nPerm=1,
-             proximity, oob.prox=proximity,
-             norm.votes=TRUE, do.trace=FALSE,
-             keep.forest=!is.null(y) && is.null(xtest), corr.bias=FALSE,
-             keep.inbag=FALSE, ...)

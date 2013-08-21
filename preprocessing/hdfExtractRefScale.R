60cabe9a601acf20a8d49fcc1f0fe65c198182db
schmingo <sschlauss@gmail.com>
2013-08-21 10:42:31 +0200

    extract emissive bands

--- preprocessing/hdfExtractRefScale.R
+++ preprocessing/hdfExtractRefScale.R
@@ -23,20 +23,24 @@ setwd(path.wd)
 hdf.info <- GDALinfo("MOD021KM.A2013188.1120.005.2013188200351.hdf", 
                      returnScaleOffset = F)
 
-# HDF attributes
-attributes(hdf.info) # display attributes
+# # HDF attributes
+# attributes(hdf.info) # display attributes
 subds <- attr(hdf.info, "subdsmdata") # display subdatasets
 
 # Identify relevant SubDS via regular expression
 refsb <- subds[grep("EOS_SWATH.*EV_1KM_RefSB$", subds)]
+emissive <- subds[grep("EOS_SWATH.*EV_1KM_Emissive$", subds)]
 # Remove irrelevant parts of SubDS name
 refsb <- unlist(strsplit(refsb, "="))[2]
-
+emissive <- unlist(strsplit(emissive, "="))[2]
 # GDALinfo from SubDS
-subds.info <- GDALinfo(refsb)
+subds.info.refsb <- GDALinfo(refsb)
+subds.info.emissive <- GDALinfo(emissive)
 
 # Extract radiance scale from SubDS metadata
-subds.mdata <- attr(subds.info, "mdata")
+subds.mdata.refsb <- attr(subds.info.refsb, "mdata")
+subds.mdata.emissive <- attr(subds.info.emissive, "mdata")
 
-scales.EV_1KM_RefSB <- subds.mdata[grep("radiance_scales", subds.mdata)]
+scales.EV_1KM_RefSB <- subds.mdata.refsb[grep("radiance_scales", subds.mdata.refsb)]
+scales.EV_1KM_Emissive <- subds.mdata.emissive[grep("radiance_scales", subds.mdata.emissive)]
 print(scales.EV_1KM_RefSB)

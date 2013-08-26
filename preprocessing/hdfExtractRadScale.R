0d0c83aa2fa3881b60b922dec1a32f71fb43045b
schmingo <sschlauss@gmail.com>
2013-08-26 11:53:47 +0200

    format edits

--- preprocessing/hdfExtractRadScale.R
+++ preprocessing/hdfExtractRadScale.R
@@ -20,16 +20,19 @@ hdfExtractRadScale <- function(path.wd,
   {
 
 
-# Required packages
+  ## Required packages
   stopifnot(require(rgdal))
+  
 
-# Data folder
+  ## Data folder
   setwd(path.wd)
 
+  
   ## GDALinfo from HDF
   info.250.hdf <- GDALinfo(path.250.hdf, returnScaleOffset = F)
   info.500.hdf <- GDALinfo(path.500.hdf, returnScaleOffset = F)
   info.1km.hdf <- GDALinfo(path.1km.hdf, returnScaleOffset = F)
+  
   
   ## HDF attributes
   #attributes(info.250.hdf) # display attributes
@@ -37,11 +40,13 @@ hdfExtractRadScale <- function(path.wd,
   subds.500 <- attr(info.500.hdf, "subdsmdata") # display subdatasets
   subds.1km <- attr(info.1km.hdf, "subdsmdata") # display subdatasets
   
+  
   ## Identify relevant SubDS via regular expression
   refsb.250 <- subds.250[grep("EOS_SWATH.*EV_250_RefSB$", subds.250)]
   refsb.500 <- subds.500[grep("EOS_SWATH.*EV_500_RefSB$", subds.500)]
   refsb.1km <- subds.1km[grep("EOS_SWATH.*EV_1KM_RefSB$", subds.1km)]
   emiss.1km <- subds.1km[grep("EOS_SWATH.*EV_1KM_Emissive$", subds.1km)]
+  
   
   ## Remove irrelevant parts of SubDS name
   refsb.250 <- unlist(strsplit(refsb.250, "="))[2]
@@ -49,11 +54,13 @@ hdfExtractRadScale <- function(path.wd,
   refsb.1km <- unlist(strsplit(refsb.1km, "="))[2]
   emiss.1km <- unlist(strsplit(emiss.1km, "="))[2]
   
+  
   ## GDALinfo from SubDS
   info.subds.refsb.250 <- GDALinfo(refsb.250)
   info.subds.refsb.500 <- GDALinfo(refsb.500)
   info.subds.refsb.1km <- GDALinfo(refsb.1km)
   info.subds.emiss.1km <- GDALinfo(emiss.1km)
+  
   
   ## Extract radiance scale from SubDS metadata
   mdata.subds.refsb.250 <- attr(info.subds.refsb.250, "mdata")
@@ -71,22 +78,15 @@ hdfExtractRadScale <- function(path.wd,
   scales.refsb.1km <- unlist(strsplit(scales.refsb.1km, "="))[2]
   scales.emiss.1km <- unlist(strsplit(scales.emiss.1km, "="))[2]
   
-  ## Print radiance scales
-  print(scales.refsb.250)
-  print(scales.refsb.500)
-  print(scales.refsb.1km)
-  print(scales.emiss.1km)
   
-  # strsplit(dein.vector, ", ")
-  # sapply(strsplit(dein.vector, ", "), as.numeric)
-  
-  
+  ## write extracted values as numeric list
   sapply(strsplit(scales.refsb.250, ", "), as.numeric)
   sapply(strsplit(scales.refsb.500, ", "), as.numeric)
   sapply(strsplit(scales.refsb.1km, ", "), as.numeric)
   sapply(strsplit(scales.emiss.1km, ", "), as.numeric)
   
-  # "es funktioniert nur 1 return() befehl!"
+  
+  ## return extracted values to call-script
   return(list(scales.refsb.250, scales.refsb.500, scales.refsb.1km, scales.emiss.1km))
 
 }

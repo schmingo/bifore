7f2f7b3049c97d44fd08e7703da6dc809f695af2
schmingo <sschlauss@gmail.com>
2013-08-27 12:00:46 +0200

    format edits

--- preprocessing/extract_vals_landsat8.R
+++ preprocessing/extract_vals_landsat8.R
@@ -1,54 +1,75 @@
 ################################################################################
-## BiFoRe Scripts
-##
-## EXTRACT GREYVALUES FROM LANDSAT 8 SATELLITE DATA USING CORNER COORDINATES
-##
-## Author: Simon Schlauss (sschlauss@gmail.com)
-## Version: 2013-08-01
-##
+## BiFoRe Scripts                                                             ##
+##                                                                            ##
+## EXTRACT GREYVALUES FROM LANDSAT 8 SATELLITE DATA USING CORNER COORDINATES  ##
+##                                                                            ##
+## Author: Simon Schlauss (sschlauss@gmail.com)                               ##
+## Version: 2013-08-01                                                        ##
+##                                                                            ##
 ################################################################################
+
 
 ## Clear workspace
 rm(list = ls(all = TRUE))
+
 
 ## Required libraries
 lib <- c("rgdal", "parallel", "raster")
 lapply(lib, function(...) require(..., character.only = TRUE))
 
-## set working directory
-setwd("/home/schmingo/Diplomarbeit/") # Linux
-#setwd("D:/Diplomarbeit/") # Windows
-#setwd("Florian")
+
+## Set filepaths and filenames
+path.wd <- "/home/schmingo/Diplomarbeit/" # Linux
+path.wd <- "D:/Diplomarbeit/" # Windows
 
 
-### Import Landsat data
+################################################################################
+### Set working directory ######################################################
+
+setwd(path.wd) # Linux
+
+
+################################################################################
+### Import Landsat data ########################################################
 
 ## List files
 files.list.sat <- list.files("src/satellite/Landsat8/hai/", 
                      pattern = ".TIF$", full.names = TRUE)
+
 
 ## Import files as RasterLayer objects
 raster.layers <- lapply(files.list.sat, raster)
 projection.layers <- CRS(projection(raster.layers[[1]]))
 
 
-### Create extends from *.csv
+################################################################################
+### Create extends from CSV-files ##############################################
 
 ## List CENTER files
-files.hai.center <- list.files("src/csv/", pattern = "hai_plot_center.csv$", full.names = TRUE)
+files.hai.center <- list.files("src/csv/", 
+                               pattern = "hai_plot_center.csv$", 
+                               full.names = TRUE)
 
 ## Import CENTER files as SpatialPointsDataframe objects
-table.hai.center <- read.csv2(files.hai.center, dec = ".", stringsAsFactors = FALSE)
+table.hai.center <- read.csv2(files.hai.center, 
+                              dec = ".", 
+                              stringsAsFactors = FALSE)
+
 coordinates(table.hai.center) <- c("Longitude", "Latitude")
 projection(table.hai.center) <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
  
 table.hai.center <- spTransform(table.hai.center, CRS = projection.layers)
 
 ## List CORNER files
-files.hai.corner <- list.files("src/csv/", pattern = "hai_corner.csv$", full.names = TRUE)
+files.hai.corner <- list.files("src/csv/", 
+                               pattern = "hai_corner.csv$", 
+                               full.names = TRUE)
 
 ## Import CORNER files as SpatialPointsDataframe objects
-table.hai <- read.csv2(files.hai.corner, dec = ".", stringsAsFactors = FALSE)
+table.hai <- read.csv2(files.hai.corner, 
+                       dec = ".", 
+                       stringsAsFactors = FALSE)
+
 coordinates(table.hai) <- c("Longitude", "Latitude")
 projection(table.hai) <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
   
@@ -60,12 +81,21 @@ extent.hai <- lapply(seq(1, nrow(table.hai), 4), function(i) {
   })
 
 
-### Extraction
+################################################################################
+### Extraction of cell values ##################################################
 
 ## Parallelization
-clstr <- makePSOCKcluster(n.cores <- 4)
-clusterExport(clstr, c("lib", "raster.layers", "extent.hai", "table.hai.center", "table.hai"))
-clusterEvalQ(clstr, lapply(lib, function(i) require(i, character.only = TRUE, quietly = TRUE)))
+clstr <- makePSOCKcluster(n.cores <- detectCores()-1
+                          )
+clusterExport(clstr, c("lib", 
+                       "raster.layers", 
+                       "extent.hai", 
+                       "table.hai.center", 
+                       "table.hai"))
+
+clusterEvalQ(clstr, lapply(lib, function(i) require(i, 
+                                                    character.only = TRUE, 
+                                                    quietly = TRUE)))
 
 ## Extract and AVERAGE cell values
 values.hai <- parLapply(clstr, raster.layers, function(h) {
@@ -85,7 +115,12 @@ values.hai <- parLapply(clstr, raster.layers, function(h) {
 
 ## Merge single data frames
 values.hai.all <- Reduce(function(...) merge(..., by = 1:6), values.hai)
-names(values.hai.all)[7:18] <- sapply(strsplit(substr(basename(files.list.sat), 1, nchar(basename(files.list.sat)) - 4), "_"), "[[", 2)
+
+names(values.hai.all)[7:18] <- sapply(strsplit(substr(basename(files.list.sat), 
+                                                      1, 
+                                                      nchar(basename(files.list.sat)) - 4), 
+                                               "_"), "[[", 2)
+
 coordinates(values.hai.all) <- c("Longitude", "Latitude")
 
 ## Deregister parallel backend
@@ -103,5 +138,9 @@ values.hai.all <- data.frame(values.hai.all)
 values.hai.all <- values.hai.all[, c(1:7,10:17,8,9,18)] 
 
 ## Write data to new csv
-write.table(values.hai.all, file = "src/csv/hai_greyvalues_landsat8.csv", dec = ".", quote = FALSE, 
-            col.names = TRUE, row.names = FALSE, sep =";")
+write.table(values.hai.all, file = "src/csv/hai_greyvalues_landsat8.csv", 
+            dec = ".", 
+            quote = FALSE, 
+            col.names = TRUE, 
+            row.names = FALSE, 
+            sep =";")

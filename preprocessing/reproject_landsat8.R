0849b7970fb157cffde8ec4de3b5ca19d87c796e
schmingo <sschlauss@gmail.com>
2013-08-29 09:59:42 +0200

    testing

--- preprocessing/reproject_landsat8.R
+++ preprocessing/reproject_landsat8.R
@@ -8,19 +8,38 @@
 ##                                                                            ##
 ################################################################################
 
-reproject_landsat8 <- function(path.wd,
-                               path.img,
-                               path.out
-                               )
-{
-  
-  print ("Reproject Landsat8 files ...")
-  
-  ## Required packages
-  stopifnot(require(rgdal, raster, parallel))
-  
-  ## Set Working Directory
-  setwd(path.wd)
+
+## Clear workspace
+rm(list = ls(all = TRUE))
+
+
+## Required libraries
+lib <- c("rgdal", "parallel", "raster")
+lapply(lib, function(...) require(..., character.only = TRUE))
+
+
+## Set filepaths and filenames
+path.wd <- "/home/schmingo/Diplomarbeit/" # Linux
+#path.wd <- "D:/Diplomarbeit/" # Windows
+
+path.img <- "src/satellite/Landsat8/hai/"
+path.out <- "src/satellite/Landsat8/hai/out"
+
+
+
+# reproject_landsat8 <- function(path.wd,
+#                                path.img,
+#                                path.out
+#                                )
+# {
+#   
+#   print ("Reproject Landsat8 files ...")
+#   
+#   ## Required packages
+#   stopifnot(require(rgdal, raster, parallel))
+#   
+#   ## Set Working Directory
+   setwd(path.wd)
   
   ##############################################################################
   ### Import Landsat8 Data #####################################################
@@ -39,15 +58,15 @@ reproject_landsat8 <- function(path.wd,
   
   ## Parallelization
   clstr <- makePSOCKcluster(n.cores <- detectCores()-1)
-  clusterExport(clstr, c("lib", "rst.ls"))
+  clusterExport(clstr, c("lib","rst.ls"))
   clusterEvalQ(clstr, lapply(lib, function(i) require(i, 
                                                       character.only = TRUE, 
                                                       quietly = TRUE)))
 
   ## Reproject and save rasters
-  rst.ls.rpj <- parLapply(clstr, rst.ls, path.out, function(i) {
+  rst.ls.rpj <- parLapply(clstr, rst.ls, function(i) {
     projectRaster(i, crs = CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"), 
-                  filename = paste(path.out, 
+                  filename = paste(path.img, 
                                   substr(basename(rst.ls[[1]]@file@name), 
                                          1, 
                                          nchar(basename(rst.ls[[1]]@file@name)) - 4), 

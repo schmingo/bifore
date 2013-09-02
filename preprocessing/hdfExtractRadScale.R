32165ccc94be844d70ce6f9d59f4693d6dec8ad4
schmingo <sschlauss@gmail.com>
2013-09-02 12:43:45 +0200

    rename path.modis to path.raw.modis

--- preprocessing/hdfExtractRadScale.R
+++ preprocessing/hdfExtractRadScale.R
@@ -13,7 +13,7 @@
 ################################################################################
 
 
-hdfExtractRadScale <- function(path.modis,
+hdfExtractRadScale <- function(path.raw.modis,
                                path.250.hdf,
                                path.500.hdf,
                                path.1km.hdf
@@ -30,11 +30,11 @@ hdfExtractRadScale <- function(path.modis,
 
   
   ## GDALinfo from HDF
-  info.250.hdf <- GDALinfo(paste(path.modis,path.250.hdf, sep = ""), 
+  info.250.hdf <- GDALinfo(paste(path..raw.modis,path.250.hdf, sep = ""), 
                            returnScaleOffset = F)
-  info.500.hdf <- GDALinfo(paste(path.modis,path.500.hdf, sep = ""), 
+  info.500.hdf <- GDALinfo(paste(path.raw.modis,path.500.hdf, sep = ""), 
                            returnScaleOffset = F)
-  info.1km.hdf <- GDALinfo(paste(path.modis,path.1km.hdf, sep = ""), 
+  info.1km.hdf <- GDALinfo(paste(path.raw.modis,path.1km.hdf, sep = ""), 
                            returnScaleOffset = F)
   
   

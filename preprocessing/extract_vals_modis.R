8f81d21e36bb2181ef226ebefca1a6f756ca5ab5
schmingo <sschlauss@gmail.com>
2013-09-03 11:22:23 +0200

    multiply radscale with modis bands

--- preprocessing/extract_vals_modis.R
+++ preprocessing/extract_vals_modis.R
@@ -150,20 +150,26 @@ print(radscales)
 greyvalues <- data.frame(greyvalues,stringsAsFactors = F)
 radscales <- data.frame(radscales, stringsAsFactors = F)
 # Spaltennamen von greyvalues in radscales übernehmen
-names(radscales) <- names(greyvalues[7:44])
+#names(radscales) <- names(greyvalues[7:44])
 
 ## merge dataframes
-henninger <- merge(greyvalues, radscales[2,], all.x=T, all.y=T)
+#henninger <- merge(greyvalues, radscales[2,], all.x=T, all.y=T)
 
 ## change column order back to normal
-henninger <- subset(henninger,
-                    select=c("Plotname","Plotid", "Status", "Location", "Longitude", 
-                  "Latitude", "B01", "B02", "B03", "B04", "B05", "B06", "B07", 
-                  "B08", "B09", "B10", "B11", "B12", "B13.1", "B13.2", "B14.1", 
-                  "B14.2", "B15", "B16", "B17", "B18", "B19", "B20", "B21",
-                  "B22", "B23", "B24", "B25", "B26", "B27", "B28", "B29", "B30",
-                  "B31", "B32", "B33", "B34", "B35", "B36")) 
-# hasseroeder <- cbind(greyvalues, radscales[2,])
+#henninger <- subset(henninger,
+#                     select=c("Plotname","Plotid", "Status", "Location", "Longitude", 
+#                   "Latitude", "B01", "B02", "B03", "B04", "B05", "B06", "B07", 
+#                   "B08", "B09", "B10", "B11", "B12", "B13.1", "B13.2", "B14.1", 
+#                   "B14.2", "B15", "B16", "B17", "B18", "B19", "B20", "B21",
+#                   "B22", "B23", "B24", "B25", "B26", "B27", "B28", "B29", "B30",
+#                   "B31", "B32", "B33", "B34", "B35", "B36")) 
+hasseroeder <- as.numeric(radscales[["scales"]])
+class(hasseroeder)
+
+veltins <- data.frame(t(t(greyvalues[7:44]) * hasseroeder))
+veltins <- greyvalues %*% diag(hasseroeder)
+
+#sweep(greyvalues[7:44], MARGIN=2, hasseroeder, '*')
 # klaustaler <- rbind.fill(greyvalues, radscales)
 # hefeweizen <- rbind(greyvalues[7:ncol(greyvalues)], radscales)
 # 

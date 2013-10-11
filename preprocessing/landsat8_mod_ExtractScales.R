4fce1bc5783d350bb826ca3d566e94ef01311748
schmingo <sschlauss@gmail.com>
2013-10-11 11:19:25 +0200

    write extracted data to new dataframe

--- preprocessing/landsat8_mod_ExtractScales.R
+++ preprocessing/landsat8_mod_ExtractScales.R
@@ -25,7 +25,7 @@ metadata <- scan(file = name.metadata,
                  sep = "")
 
 ################################################################################
-## Extract Reflectance Scale ###################################################
+## Extract reflectance- and radiance Scales ####################################
 
 refscale.b1 <- as.numeric(metadata[grep("REFLECTANCE_MULT_BAND_1", metadata)+2])
 refscale.b2 <- as.numeric(metadata[grep("REFLECTANCE_MULT_BAND_2", metadata)+2])
@@ -42,7 +42,7 @@ radscale.b11 <- as.numeric(metadata[grep("RADIANCE_MULT_BAND_11", metadata)+2])
 
 
 ################################################################################
-## Extract Reflectance Offset ##################################################
+## Extract reflectance- and radiance offsets ###################################
 
 refoffset.b1 <- as.numeric(metadata[grep("REFLECTANCE_ADD_BAND_1", metadata)+2])
 refoffset.b2 <- as.numeric(metadata[grep("REFLECTANCE_ADD_BAND_2", metadata)+2])
@@ -56,3 +56,48 @@ refoffset.b9 <- as.numeric(metadata[grep("REFLECTANCE_ADD_BAND_9", metadata)+2])
 
 radoffset.b10 <- as.numeric(metadata[grep("RADIANCE_ADD_BAND_10", metadata)+2])
 radoffset.b11 <- as.numeric(metadata[grep("RADIANCE_ADD_BAND_11", metadata)+2])
+
+
+################################################################################
+## Write scales and offset to single dataframe #################################
+
+scales <- paste(refscale.b1,
+                refscale.b2,
+                refscale.b3,
+                refscale.b4,
+                refscale.b5,
+                refscale.b6,
+                refscale.b7,
+                refscale.b8,
+                refscale.b9,
+                radscale.b10,
+                radscale.b11,
+                sep = ", ")
+
+offset <- paste(refoffset.b1,
+                refoffset.b2,
+                refoffset.b3,
+                refoffset.b4,
+                refoffset.b5,
+                refoffset.b6,
+                refoffset.b7,
+                refoffset.b8,
+                refoffset.b9,
+                radoffset.b10,
+                radoffset.b11,
+                sep = ", ")
+
+
+## Write bandnames, radiance- /reflectance scales and offset to separate dataframes
+
+bandnames <- data.frame(c("B01", "B02", "B03", "B04", "B05", "B06", "B07", "B08", "B09", "B10", "B11"))
+names(bandnames) <- "bands"
+
+scales <- data.frame(strsplit(unlist(scales), ","),stringsAsFactors = F)
+names(scales) <- "scales"
+
+offset <- data.frame(strsplit(unlist(offset), ","),stringsAsFactors = F)
+names(offset) <- "offset"
+
+
+metadata.ls8 <- cbind(bandnames, scales, offset)

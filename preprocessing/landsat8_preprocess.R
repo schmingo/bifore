2922f63a3b68ee51d0384f85d79d9677a191d1ba
schmingo <sschlauss@gmail.com>
2013-10-12 13:32:44 +0200

    format edits

--- preprocessing/landsat8_preprocess.R
+++ preprocessing/landsat8_preprocess.R
@@ -28,13 +28,11 @@ path.wd <- "/home/schmingo/Dropbox/Diplomarbeit/code/bifore/"
 path.img <- "src/satellite/Landsat8/hai/"
 #path.out <- "src/satellite/Landsat8/hai/out/" (only necessary for reprojection)
 
-## Filepath to input csv files
-path.csv <- "src/csv/hai/"
-
 path.modules <- "/home/schmingo/Diplomarbeit/bifore/"
 filename.mod.ExtractScales <- "preprocessing/landsat8_mod_ExtractScales.R"
 
 ## Filepath and filename of output csv
+path.csv <- "src/csv/hai/"
 csv.out.raw <- "hai_greyvalues_RAW.csv"
 csv.out <- "hai_greyvalues.csv"
 csv.out.NA <- "hai_greyvalues_NA.csv"

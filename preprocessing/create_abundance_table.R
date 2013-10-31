172e56eb0c879048721f75c94095821a1d0596b0
schmingo <sschlauss@gmail.com>
2013-10-31 09:54:05 +0100

    update for kili dataset

--- preprocessing/create_abundance_table.R
+++ preprocessing/create_abundance_table.R
@@ -5,7 +5,7 @@
 ##                                                                            ##
 ##                                                                            ##
 ## Author: Simon Schlauss (sschlauss@gmail.com)                               ##
-## Version: 2013-10-07                                                        ##
+## Version: 2013-10-31                                                        ##
 ##                                                                            ##
 ################################################################################
 
@@ -14,17 +14,14 @@ rm(list = ls(all = TRUE))
 
 
 ## Required libraries
-lib <- c()
-lapply(lib, function(...) require(..., character.only = TRUE))
+# lib <- c()
+# lapply(lib, function(...) require(..., character.only = TRUE))
 
 
 ## Set filepaths and filenames
 path.wd <- "/home/schmingo/Dropbox/Diplomarbeit/code/bifore/src/"
 
-path.abundance.out <- "csv/all_abundance.csv"
-path.abundance.out.alb <- "csv/alb/alb_abundance.csv"
-path.abundance.out.hai <- "csv/hai/hai_abundance.csv"
-path.abundance.out.sch <- "csv/sch/sch_abundance.csv"
+path.abundance.out <- "csv/kili/kili_abundance.csv"
 
 
 ################################################################################
@@ -35,7 +32,7 @@ setwd(path.wd)
 ################################################################################
 ### Import MODIS data ##########################################################
 
-data <- read.csv2("csv/MODIS_20130707-1120_RAW.csv",
+data <- read.csv2("csv/kili/MODIS_20060129_greyvalues_RAW.csv",
                    dec = ".",
                    stringsAsFactors = FALSE)
 
@@ -43,7 +40,7 @@ data <- read.csv2("csv/MODIS_20130707-1120_RAW.csv",
 ################################################################################
 ### Subsetting #################################################################
 
-data.sub <- data[,1:6]
+data.sub <- data[,1:5]
 
 
 ################################################################################
@@ -59,36 +56,6 @@ data.abundance <- cbind(data.sub,
 ### Write abundance tables #####################################################
 
 write.table(data.abundance, file = path.abundance.out, 
-            dec = ".", 
-            quote = FALSE, 
-            col.names = TRUE, 
-            row.names = FALSE, 
-            sep =";")
-
-## Write alb table
-data.alb.abundance <- data.abundance[1:100,]
-write.table(data.alb.abundance,
-            file = path.abundance.out.alb, 
-            dec = ".", 
-            quote = FALSE, 
-            col.names = TRUE, 
-            row.names = FALSE, 
-            sep =";")
-
-## Write hai table
-data.hai.abundance <- data.abundance[101:200,]
-write.table(data.hai.abundance,
-            file = path.abundance.out.hai, 
-            dec = ".", 
-            quote = FALSE, 
-            col.names = TRUE, 
-            row.names = FALSE, 
-            sep =";")
-
-## Write sch table
-data.sch.abundance <- data.abundance[201:300,]
-write.table(data.sch.abundance,
-            file = path.abundance.out.sch, 
             dec = ".", 
             quote = FALSE, 
             col.names = TRUE, 

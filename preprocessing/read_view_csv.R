719c9e9e3a5cb762fd1eef71785fb558f6d50f67
schmingo <sschlauss@gmail.com>
2013-11-26 10:21:42 +0100

    removed reading .sav file - not necessary

--- preprocessing/read_view_csv.R
+++ preprocessing/read_view_csv.R
@@ -4,7 +4,7 @@
 ## Import and view csv files                                                  ##
 ##                                                                            ##
 ## Author: Simon Schlauss (sschlauss@gmail.com)                               ##
-## Version: 2013-11-18                                                        ##
+## Version: 2013-11-26                                                        ##
 ##                                                                            ##
 ################################################################################
 
@@ -12,7 +12,7 @@
 rm(list = ls(all = TRUE))
 
 ## Required libraries
-lib <- c("foreign", "Hmisc")
+lib <- c()
 lapply(lib, function(...) require(..., character.only = TRUE))
 
 
@@ -22,7 +22,6 @@ setwd("D:/Dropbox/Diplomarbeit/code/bifore/")
 
 ## set filepaths
 file.abundance.csv <- "src/csv/kili/abundance_matrix_hemp.csv"
-file.abundance.sav <- "src/csv/kili/abundance_matrix_hemp.sav"
 
 ## read data
 data.csv <- read.csv2(file.abundance.csv, 
@@ -33,5 +32,3 @@ data.csv <- read.csv2(file.abundance.csv,
                      stringsAsFactors = FALSE)
 
 
-# data.sav <- read.spss(file.abundance.sav)
-# data.sav <- spss.get(file.abundance.sav)

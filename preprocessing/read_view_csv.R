ec4a08e131687c04fcab68647c31c95e6255ce7e
schmingo <sschlauss@gmail.com>
2013-11-26 10:48:21 +0100

    replace 0-values with NA's

--- preprocessing/read_view_csv.R
+++ preprocessing/read_view_csv.R
@@ -38,3 +38,12 @@ data <- read.csv2(file.abundance.csv,
 data$date <- as.Date(data$date, format="%m/%d/%Y")
 
 ################################################################################
+## replace 0-values with NA
+
+# subset data
+data.species <- data[,9:ncol(data)]
+
+# set 0-values to NA
+data.species[data.species==0] <- NA
+
+data[,9:ncol(data)] <- data.species

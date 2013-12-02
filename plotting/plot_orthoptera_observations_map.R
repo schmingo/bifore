d7380d2632ab42792a775dd7425e3d6fdfc4867e
schmingo <sschlauss@gmail.com>
2013-12-02 15:38:46 +0100

    add facet plot (split by year)

--- plotting/plot_orthoptera_observations_map.R
+++ plotting/plot_orthoptera_observations_map.R
@@ -29,6 +29,7 @@ data <- read.csv2("csv/kili/abundance_data_subset.csv",
                      stringsAsFactors = TRUE,
                      )
 
+data$date <- as.Date(data$date, format="%Y-%m-%d")
 data.sp <- data
 ################################################################################
 ### Transform UTM to LatLong Coordinates #######################################
@@ -51,11 +52,15 @@ data <- data.sp
 
 data.species <- data[,1:9]
 
+data.species$year <- as.numeric(format(data.species$date, "%Y"))
+
+
 ################################################################################
 ### Plots ######################################################################
 
-
+### Plot all orthoptera observations ###########################################
 ## Define output image | open image port
+
 png("images/map_kili_orthoptera_observations.png", 
     width = 1024 * 6, 
     height = 748 * 6, 
@@ -92,6 +97,50 @@ colourscale <- scale_colour_gradient(low = "white",
 
 
 kilimap + orthoptera.obs1 + colourscale
+
+
+## Close image port
+graphics.off()
+
+### Plot Orthoptera Observations per year ######################################
+## Define output image | open image port
+png("images/map_kili_orthoptera_observations_year.png", 
+    width = 1024 * 6, 
+    height = 748 * 6, 
+    units = "px", 
+    res = 600)
+
+
+kili.extent <- get_map(location = c(36.93865,
+                                    -3.454621,
+                                    37.76235,
+                                    -2.775392),
+                       #                        zoom = 11,
+                       scale = "auto",
+                       maptype = "satellite",
+                       color = "bw",
+                       source = "google")
+
+kilimap <- ggmap(kili.extent, 
+                 extent = "normal",
+                 maprange = TRUE)
+
+
+orthoptera.obs1 <- geom_point(aes(x = lon,
+                                  y = lat,
+#                                   size = 1,
+                                  colour = nr.of.species),
+                              show_guide = FALSE,
+                              data = data.species)
+
+colourscale <- scale_colour_gradient(low = "white", 
+                                     high = "darkgreen", 
+                                     name = "Number of species"
+)
+
+
+kilimap + orthoptera.obs1 + colourscale + facet_wrap(~ year)
+
 
 ## Close image port
 graphics.off()

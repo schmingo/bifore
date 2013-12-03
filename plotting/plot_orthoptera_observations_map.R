9962291899012be0a01e2303e97bd9b72dcec6ec
schmingo <sschlauss@gmail.com>
2013-12-03 09:32:16 +0100

    format edits

--- plotting/plot_orthoptera_observations_map.R
+++ plotting/plot_orthoptera_observations_map.R
@@ -29,8 +29,12 @@ data <- read.csv2("csv/kili/abundance_data_subset.csv",
                      stringsAsFactors = TRUE,
                      )
 
+## Read date column as a date
 data$date <- as.Date(data$date, format="%Y-%m-%d")
+
 data.sp <- data
+
+
 ################################################################################
 ### Transform UTM to LatLong Coordinates #######################################
 
@@ -40,18 +44,25 @@ data.sp <- spTransform(data.sp, CRS("+proj=longlat"))
 
 data.sp <- as.data.frame(data.sp)
 
+## Recombine dataframes
 data.sp <- cbind(data[1:3], 
                  data.sp$coordW, 
                  data.sp$coordN,
                  data[6:ncol(data)])
 
+## Set colnames
 names(data.sp)[4] <- "lon"
 names(data.sp)[5] <- "lat"
 
 data <- data.sp
 
+
+################################################################################
+### Subsetting #################################################################
+
 data.species <- data[,1:9]
 
+## Create additional year column
 data.species$year <- as.numeric(format(data.species$date, "%Y"))
 
 
@@ -59,20 +70,18 @@ data.species$year <- as.numeric(format(data.species$date, "%Y"))
 ### Plots ######################################################################
 
 ### Plot all orthoptera observations ###########################################
-## Define output image | open image port
 
+## Define output image | open image port
 png("images/map_kili_orthoptera_observations.png", 
     width = 1024 * 6, 
     height = 748 * 6, 
     units = "px", 
     res = 600)
 
-
 kili.extent <- get_map(location = c(36.93865,
                                     -3.454621,
                                     37.76235,
                                     -2.775392),
-#                        zoom = 11,
                        scale = "auto",
                        maptype = "satellite",
                        color = "bw",
@@ -81,7 +90,6 @@ kili.extent <- get_map(location = c(36.93865,
 kilimap <- ggmap(kili.extent, 
                  extent = "normal",
                  maprange = TRUE)
-
 
 orthoptera.obs1 <- geom_point(aes(x = lon,
                                   y = lat,
@@ -103,6 +111,7 @@ kilimap + orthoptera.obs1 + colourscale
 graphics.off()
 
 ### Plot Orthoptera Observations per year ######################################
+
 ## Define output image | open image port
 png("images/map_kili_orthoptera_observations_year.png", 
     width = 1024 * 6, 
@@ -115,7 +124,6 @@ kili.extent <- get_map(location = c(36.93865,
                                     -3.454621,
                                     37.76235,
                                     -2.775392),
-                       #                        zoom = 11,
                        scale = "auto",
                        maptype = "satellite",
                        color = "bw",
@@ -125,10 +133,8 @@ kilimap <- ggmap(kili.extent,
                  extent = "normal",
                  maprange = TRUE)
 
-
 orthoptera.obs1 <- geom_point(aes(x = lon,
                                   y = lat,
-#                                   size = 1,
                                   colour = nr.of.species),
                               show_guide = FALSE,
                               data = data.species)
@@ -136,8 +142,7 @@ orthoptera.obs1 <- geom_point(aes(x = lon,
 colourscale <- scale_colour_gradient(low = "white", 
                                      high = "darkgreen", 
                                      name = "Number of species"
-)
-
+                                     )
 
 kilimap + orthoptera.obs1 + colourscale + facet_wrap(~ year)
 

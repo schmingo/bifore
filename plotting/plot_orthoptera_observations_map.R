5f42911c30b85c7b68fadd3dacf7b7266addfe02
schmingo <sschlauss@gmail.com>
2013-12-03 16:24:13 +0100

    add plot for single species

--- plotting/plot_orthoptera_observations_map.R
+++ plotting/plot_orthoptera_observations_map.R
@@ -127,3 +127,49 @@ kilimap + orthoptera.obs1 + colourscale + facet_wrap(~ year)
 
 # ## Close image port
 # graphics.off()
+
+### Plot Phaneroptera.sparsa ###################################################
+
+## Subset
+data.species1 <- data
+data.species1$year <- as.numeric(format(data.species1$date, "%Y"))
+data.species1 <- cbind(data.species1[1:11],
+                       data.species1[ncol(data.species1)],
+                       data.species1$Phaneroptera.sparsa)
+
+names(data.species1)[13] <- "Phaneroptera.sparsa"
+
+data.species1.1 <- na.omit(data.species1)
+
+## Define output image | open image port
+# png("images/map_kili_Phaneroptera.sparsa_year.png", 
+#     width = 1024 * 6, 
+#     height = 748 * 6, 
+#     units = "px", 
+#     res = 600)
+
+kili.extent <- get_map(location = c(36.93865,
+                                    -3.454621,
+                                    37.76235,
+                                    -2.775392),
+                       scale = "auto",
+                       maptype = "satellite",
+                       color = "bw",
+                       source = "google")
+
+kilimap <- ggmap(kili.extent, 
+                 extent = "normal",
+                 maprange = TRUE)
+
+species1.1 <- geom_point(aes(x = lon,
+                             y = lat,
+                             colour = as.factor(Phaneroptera.sparsa)),
+                         show_guide = TRUE,
+                         data = data.species1.1)
+
+colourscale <- scale_colour_brewer(type = "qual", palette = 2, name = "Prävalenz Phaneroptera.sparsa")
+
+kilimap + species1.1 + colourscale + facet_wrap(~ year)
+
+# ## Close image port
+# graphics.off()

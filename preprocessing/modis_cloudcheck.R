14eb565b4882e9cfb36d6a15c97bfda3ec219be8
schmingo <sschlauss@gmail.com>
2013-12-09 12:02:31 +0100

    disable further processing - try write_MRTSwath_param_file and run_swath2grid functions without external scripts

--- preprocessing/modis_cloudcheck.R
+++ preprocessing/modis_cloudcheck.R
@@ -82,52 +82,73 @@ for (i in 1:nrow(fns_df)) {
 #                   "Cloud_Water_Path", 
 #                   "Cirrus_Reflectance", "Cirrus_Reflectance_Flag"), 
 #           .packages = "modiscloud") %do% {
-            
-            prmfn = writeMRTSwathParamFile(prmfn = "tmpMRTparams.prm", 
-                                           tifsdir = tifsdir, 
-                                           modfn = fns_df$mod35_L2_fns[i], 
-                                           geoloc_fn = fns_df$mod03_fns[i], 
-#                                            sds = sds, 
-                                           ul_lon = ul_lon, ul_lat = ul_lat, 
-                                           lr_lon = lr_lon, lr_lat = lr_lat)
-            
-            runSwath2Grid(mrtpath = mrtpath, 
-                          prmfn = "tmpMRTparams.prm", 
-                          tifsdir = tifsdir, 
-                          modfn = fns_df$mod35_L2_fns[i], 
-                          geoloc_fn = fns_df$mod03_fns[i], 
-                          ul_lon = ul_lon, ul_lat = ul_lat, lr_lon = lr_lon, lr_lat = lr_lat)
-          }
+  prmfn = write_MRTSwath_param_file(prmfn="tmpMRTparams.prm", 
+                                    tifsdir=tifsdir, 
+                                    modfn=fns_df$mod35_L2_fns[i], 
+                                    geoloc_fn=fns_df$mod03_fns[i], 
+                                    ul_lon=ul_lon, 
+                                    ul_lat=ul_lat, 
+                                    lr_lon=lr_lon, 
+                                    lr_lat=lr_lat)
+  
+  print(scan(file=prmfn, what="character", sep="\n"))           
+}
+
+run_swath2grid(mrtpath="swath2grid", 
+               prmfn="tmpMRTparams.prm", 
+               tifsdir=tifsdir, 
+               modfn=fns_df$mod35_L2_fns[i], 
+               geoloc_fn=fns_df$mod03_fns[i], 
+               ul_lon=ul_lon, 
+               ul_lat=ul_lat, 
+               lr_lon=lr_lon, 
+               lr_lat=lr_lat)
+
+#             prmfn = writeMRTSwathParamFile(prmfn = "tmpMRTparams.prm", 
+#                                            tifsdir = tifsdir, 
+#                                            modfn = fns_df$mod35_L2_fns[i], 
+#                                            geoloc_fn = fns_df$mod03_fns[i], 
+# #                                            sds = sds, 
+#                                            ul_lon = ul_lon, ul_lat = ul_lat, 
+#                                            lr_lon = lr_lon, lr_lat = lr_lat)
+#             
+#             runSwath2Grid(mrtpath = mrtpath, 
+#                           prmfn = "tmpMRTparams.prm", 
+#                           tifsdir = tifsdir, 
+#                           modfn = fns_df$mod35_L2_fns[i], 
+#                           geoloc_fn = fns_df$mod03_fns[i], 
+#                           ul_lon = ul_lon, ul_lat = ul_lat, lr_lon = lr_lon, lr_lat = lr_lat)
+#           }
 # }
 
-tiffns = list.files(tifsdir, pattern = ".tif", full.names = TRUE)
-tiffns
-
-clusterEvalQ(cl, library(raster))
-clusterExport(cl, "tiffns")
-
-tiffrst <- parLapply(cl, tiffns, function(x) { 
-  rst <- raster(x)
-  
-  rst.prj <- projectRaster(rst, crs = "+proj=utm +zone=37 +south +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
-  
-  if (ifelse(length(grep("Cloud_Effective_Radius", x) == 1) > 0, T, F) |
-      ifelse(length(grep("Cloud_Optical_Thickness", x) == 1) > 0, T, F)) {
-    scale.factor <- 0.009999999776482582
-  } else if (ifelse(length(grep("Cirrus_Reflectance", x) == 1) > 0, T, F)) {
-    scale.factor <- 1.9999999494757503e-4
-  } else {
-    scale.factor <- 1
-  }
-  
-  rst.prj.scl <- rst.prj * scale.factor
-  
-  writeRaster(rst.prj.scl, paste("tifs/prj_scl", basename(x), sep = "/"), 
-              format = "GTiff", overwrite = T)
-  
-#   rst.prj.scl.fcl <- focal(rst.prj.scl, w = 5, fun = sd, 
-#                            filename = paste("tifs/prj_scl_fcl", basename(x), sep = "/"), 
-#                            format = "GTiff", overwrite = T)
-})
-
-stopCluster(cl)
+# tiffns = list.files(tifsdir, pattern = ".tif", full.names = TRUE)
+# tiffns
+# 
+# clusterEvalQ(cl, library(raster))
+# clusterExport(cl, "tiffns")
+# 
+# tiffrst <- parLapply(cl, tiffns, function(x) { 
+#   rst <- raster(x)
+#   
+#   rst.prj <- projectRaster(rst, crs = "+proj=utm +zone=37 +south +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
+#   
+#   if (ifelse(length(grep("Cloud_Effective_Radius", x) == 1) > 0, T, F) |
+#       ifelse(length(grep("Cloud_Optical_Thickness", x) == 1) > 0, T, F)) {
+#     scale.factor <- 0.009999999776482582
+#   } else if (ifelse(length(grep("Cirrus_Reflectance", x) == 1) > 0, T, F)) {
+#     scale.factor <- 1.9999999494757503e-4
+#   } else {
+#     scale.factor <- 1
+#   }
+#   
+#   rst.prj.scl <- rst.prj * scale.factor
+#   
+#   writeRaster(rst.prj.scl, paste("tifs/prj_scl", basename(x), sep = "/"), 
+#               format = "GTiff", overwrite = T)
+#   
+# #   rst.prj.scl.fcl <- focal(rst.prj.scl, w = 5, fun = sd, 
+# #                            filename = paste("tifs/prj_scl_fcl", basename(x), sep = "/"), 
+# #                            format = "GTiff", overwrite = T)
+# })
+# 
+# stopCluster(cl)

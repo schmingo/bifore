75b15bb96af8ccd97b3b1141c6f77f72fe88e199
fdetsch <flowla88@gmail.com>
2014-01-09 15:19:54 +0100

    Update: provided functionality to obtain closest cloud-free MODIS scene to a given set of records.

--- preprocessing/modis_cloudcheck_testing.R
+++ preprocessing/modis_cloudcheck_testing.R
@@ -15,7 +15,7 @@
 rm(list = ls(all = TRUE))
 
 ## Required libraries
-lib <- c("modiscloud", "devtools", "doParallel", "rgdal", "foreach")
+lib <- c("modiscloud", "devtools", "doParallel", "rgdal", "raster")
 lapply(lib, function(...) require(..., character.only = TRUE))
 
 ## Set working directory
@@ -42,7 +42,8 @@ data <- read.csv2(path.csv,
 
 ## Read date column as a date
 #data$date <- as.Date(data$date, format="%Y-%m-%d")
-data$date <- as.POSIXct(data$date, format="%Y-%m-%d")
+#data$date <- as.POSIXct(data$date, format="%Y-%m-%d")
+data$date <- strftime(as.POSIXct(data$date, format="%Y-%m-%d"), format = "%Y%j")
 
 str(data[1:15])
 
@@ -53,6 +54,69 @@ str(data[1:15])
 ## get .tif list from swath2grid output
 tiffns <- list.files(path.tif.cloudmask, pattern=".tif", full.names=TRUE)
 tiffns
+
+###
+
+# Parallelization
+registerDoParallel(cl <- makeCluster(4)
+# Convert data to spatial object
+coordinates(data) <- ~ lon + lat
+proj4string(data) <- CRS("+init=epsg:4326")
+
+# Loop through all available dates
+foreach(g = 1:nrow(data), .packages = lib) %dopar% {
+  
+  print(g)
+  
+  # Initialize while-loop
+  current.date <- data$date[g]
+  cloudy <- TRUE
+  
+  while (cloudy) {
+    
+    # List available *_b0.tif files of current date
+    fls.avl <- tiffns[grep(current.date, tiffns)]
+    fls.avl.b0 <- fls.avl[grep("b0", fls.avl)]
+    
+    # Import raster images
+    rst.avl.b0 <- lapply(fls.avl.b0, raster)
+    
+    for (h in rst.avl.b0) {
+      # Extract corresponding cell values from raster images
+      val.avl.b0 <- extract(h, data[g, ])
+      # Convert extracted values to binary format  
+      bit.avl.b0 <- rev(t(digitsBase(val.avl.b0, base = 2, 8)))
+      
+      ## Cloud Indicator from MOD35 metadata
+      # Unobstructed FOV Quality Flag
+      # 00 = Cloudy                   
+      # 01 = Uncertain                
+      # 10 = Probably  Clear          
+      # 11 = Confident  Clear 
+      
+      # Break out of for-loop in case of cloud absence
+      if (!all(bit.avl.b0[2:3] == c(1, 1))) {
+        cloudy <- FALSE
+        break
+      }  
+    }
+    
+    # Increment date by 1 day in case of no cloud absence
+    if (cloudy) {
+      current.date <- as.Date(current.date, format = "%Y%j") + 1
+      current.date <- strftime(current.date, format = "%Y%j")
+    }
+  }
+  
+  # Retrieve information about first cloud-free day
+  return(paste0(dirname(fls.avl)[1], "/", names(h), ".tif"))
+}
+                   
+# Deregister parallel backend                   
+stopCluster(cl)
+                   
+###
+
 
 ## Define image for pixel extraction and cloud identification 
 fn <- tiffns[1]

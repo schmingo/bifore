bb624e524aec1aff83c36ece2e21127d10450b26
schmingo <sschlauss@gmail.com>
2014-01-10 10:59:30 +0100

    Big Update: Loop all observations from .csv, check date and cloudiness from extracted MOD35 .tifs and return all cloud-free dates.

--- preprocessing/modis_cloudcheck.R
+++ preprocessing/modis_cloudcheck.R
@@ -48,7 +48,10 @@ data <- read.csv2(path.csv,
 
 ## Read date column as a date
 #data$date <- as.Date(data$date, format="%Y-%m-%d")
-data$date <- as.POSIXct(data$date, format="%Y-%m-%d")
+#data$date <- as.POSIXct(data$date, format="%Y-%m-%d")
+data$date <- strftime(as.POSIXct(data$date, format="%Y-%m-%d"), format = "%Y%j")
+
+str(data[1:15])
 
 
 ################################################################################
@@ -102,60 +105,76 @@ for(i in 1:nrow(fns_df)) {
 
 
 ################################################################################
-### Extract values from a particular pixel #####################################
+### Check observations from csv file for cloudiness using tiffs ################
 
 ## get .tif list from swath2grid output
 tiffns <- list.files(path.tif.cloudmask, pattern=".tif", full.names=TRUE)
 tiffns
 
-## Define image for pixel extraction and cloud identification 
-fn <- tiffns[1]
+###
 
-## Define image as SpatialGridDataFrame
-grd <- readGDAL(fn)
+# Parallelization
+registerDoParallel(cl <- makeCluster(4)
+                   
+# Convert data to spatial object
+coordinates(data) <- ~ lon + lat
+proj4string(data) <- CRS("+init=epsg:4326")
+                   
+# Loop through all available dates
+foreach(g = 1:nrow(data), .packages = lib) %dopar% {
+  
+  print(g)
+  
+  # Initialize while-loop
+  current.date <- data$date[g]
+  cloudy <- TRUE
+  
+  
+  while (cloudy) {
+    
+    # List available *_b0.tif files of current date
+    fls.avl <- tiffns[grep(current.date, tiffns)]
+    fls.avl.b0 <- fls.avl[grep("b0", fls.avl)]
+    
+    # Import raster images
+    rst.avl.b0 <- lapply(fls.avl.b0, raster)
+    
+    for (h in rst.avl.b0) {
+      
+      # Extract corresponding cell values from raster images
+      val.avl.b0 <- extract(h, data[g, ])
+      
+      # Convert extracted values to binary format
+      bit.avl.b0 <- rev(t(digitsBase(val.avl.b0, base = 2, 8)))
+      
+      ## Cloud Indicator from MOD35 metadata
+      # Unobstructed FOV Quality Flag
+      # 00 = Cloudy
+      # 01 = Uncertain
+      # 10 = Probably  Clear
+      # 11 = Confident  Clear
+      
+      # Break out of for-loop in case of cloud absence
+      if (!all(bit.avl.b0[2:3] == c(0, 0))) {
+        cloudy <- FALSE
+        break
+      }
+    }
+    
+    # Increment date by 1 day in case of no cloud absence
+    if (cloudy) {
+      
+      current.date <- as.Date(current.date, format = "%Y%j") + 1
+      current.date <- strftime(current.date, format = "%Y%j")
+    }
+  }
+  
+  # Retrieve information about first cloud-free day
+  return(paste0(dirname(fls.avl)[1], "/", names(h), ".tif"))
+}
 
-## Get CRS
-grdproj <- CRS(proj4string(grd))
-# grdproj
-# grdbbox <- attr(grd, "bbox")
-# grdbbox
-
-# Get coordinates from a single observation
-data.lat <- data$lat[35]
-data.lon <- data$lon[35]
-
-## Define image as raster
-grdr <- raster(grd)
-
-## Input the points x (longitude), then y (latitude)
-point_to_sample <- c(data.lon, data.lat)
-xycoords <- adf(matrix(data = point_to_sample, nrow = 1, ncol = 2))
-names(xycoords) <- c("x", "y")
-
-xy <- SpatialPoints(coords=xycoords, proj4string=grdproj)
-
-## Extract single pixel from raster image
-pixelval <- extract(grdr, xy, buffer = NULL)
-
-# Have to convert to 8-bit binary string, and reverse to get the count correct
-# (also reverse the 2-bit strings in the MODIS Cloud Mask table)
-pixelval <- rev(t(digitsBase(pixelval, base= 2, 8)))
-
-print(pixelval)
-
-## Extract cloud indicator
-cloud_indicator <- pixelval[2:3]
-print(cloud_indicator)
-
-
-################################################################################
-### Get Date from MODIS files ##############################################
-
-tiffns <- list.files(path.b0.cloudmask, pattern=".tif", full.names=TRUE)
-tiffns
-
-mod.dates.df <- dates_from_fileslist(tiffns)
-mod.dates.df
+# Deregister parallel backend
+stopCluster(cl)
 
 ################################################################################
 ### Download MODIS MOD02 files #################################################

301307e7ea630c80ba1b4204655f1c907e3fbfc8
schmingo <sschlauss@gmail.com>
2014-01-17 10:32:13 +0100

    test file.rename

--- preprocessing/modis_cloudcheck_testing.R
+++ preprocessing/modis_cloudcheck_testing.R
@@ -15,172 +15,52 @@
 rm(list = ls(all = TRUE))
 
 ## Required libraries
-lib <- c("modiscloud", "devtools", "doParallel", "rgdal", "raster")
+lib <- c("modiscloud" ,"doParallel")
+
 lapply(lib, function(...) require(..., character.only = TRUE))
 
 ## Set working directory
 setwd("/home/schmingo/Dropbox/Diplomarbeit/code/bifore/src/")
-# setwd("/media/permanent/programming/r/bifore")
 
 ################################################################################
 ### Set filepaths ##############################################################
 
-path.csv <- ("/home/schmingo/Dropbox/Diplomarbeit/code/bifore/src/csv/kili/abundance_data_subset.csv") # update this path
-# path.csv <- ("abundance_data_subset.csv")
-
-path.tif.cloudmask <- ("/home/schmingo/Dropbox/Diplomarbeit/code/bifore/src/satellite/sample_modiscloud_out/2002-2003/") # update this path
-# path.tif.cloudmask <- ("sample_modiscloud_out/2002-2003")
+path.hdf.in <- ("/home/schmingo/SAVE/Diplomarbeit/myd03_myd35/")
+path.hdf.sub <- ("/home/schmingo/SAVE/Diplomarbeit/sub_myd03_myd35/")
 
 
 ################################################################################
-### Import dataset #############################################################
-
-data <- read.csv2(path.csv,
-                  dec = ".",
-                  header = TRUE, 
-                  stringsAsFactors = TRUE)
-
-## Read date column as a date
-#data$date <- as.Date(data$date, format="%Y-%m-%d")
-#data$date <- as.POSIXct(data$date, format="%Y-%m-%d")
-data$date <- strftime(as.POSIXct(data$date, format="%Y-%m-%d"), format = "%Y%j")
-
-str(data[1:15])
+### Separate day and night hdf files ###########################################
 
 
-################################################################################
-### Extract values from a particular pixel #####################################
+fls.myd03 <- list.files(path.hdf.in,
+                        pattern="MYD03",
+                        full.names=TRUE)
 
-## get .tif list from swath2grid output
-tiffns <- list.files(path.tif.cloudmask, pattern=".tif", full.names=TRUE)
-tiffns
+fls.myd35 <- list.files(path.hdf.in,
+                        pattern="MYD35",
+                        full.names=TRUE)
 
-###
 
-# Parallelization
-registerDoParallel(cl <- makeCluster(4)
-# Convert data to spatial object
-coordinates(data) <- ~ lon + lat
-proj4string(data) <- CRS("+init=epsg:4326")
+## MYD03
+registerDoParallel(cl <- makeCluster(4))
 
-# Loop through all available dates
-foreach(g = 1:nrow(data), .packages = lib) %dopar% {
+foreach(i in fls.myd03), .packages = lib) %dopar% {
   
-  print(g)
-  
-  # Initialize while-loop
-  current.date <- data$date[g]
-  cloudy <- TRUE
-  
-  while (cloudy) {
-    
-    # List available *_b0.tif files of current date
-    fls.avl <- tiffns[grep(current.date, tiffns)]
-    fls.avl.b0 <- fls.avl[grep("b0", fls.avl)]
-    
-    # Import raster images
-    rst.avl.b0 <- lapply(fls.avl.b0, raster)
-    
-    for (h in rst.avl.b0) {
-      # Extract corresponding cell values from raster images
-      val.avl.b0 <- extract(h, data[g, ])
-      # Convert extracted values to binary format  
-      bit.avl.b0 <- rev(t(digitsBase(val.avl.b0, base = 2, 8)))
-      
-      ## Cloud Indicator from MOD35 metadata
-      # Unobstructed FOV Quality Flag
-      # 00 = Cloudy                   
-      # 01 = Uncertain                
-      # 10 = Probably  Clear          
-      # 11 = Confident  Clear 
-      
-      # Break out of for-loop in case of cloud absence
-      if (!all(bit.avl.b0[2:3] == c(0, 0))) {
-        cloudy <- FALSE
-        break
-      }  
-    }
-    
-    # Increment date by 1 day in case of no cloud absence
-    if (cloudy) {
-      current.date <- as.Date(current.date, format = "%Y%j") + 1
-      current.date <- strftime(current.date, format = "%Y%j")
-    }
-  }
-  
-  # Retrieve information about first cloud-free day
-  return(paste0(dirname(fls.avl)[1], "/", names(h), ".tif"))
-}
-                   
-# Deregister parallel backend                   
+  if (as.numeric(substr(basename(fls.myd03[i]),16,19)) > 1700)
+    file.rename(from = fls.myd03[i],
+                to = paste0(path.hdf.sub, basename(fls.myd03[i])))
+
 stopCluster(cl)
-                   
-###
 
 
-# ## Define image for pixel extraction and cloud identification 
-# fn <- tiffns[1]
-# 
-# ## Define image as SpatialGridDataFrame
-# grd <- readGDAL(fn)
-# 
-# ## Get CRS
-# grdproj <- CRS(proj4string(grd))
-# # grdproj
-# # grdbbox <- attr(grd, "bbox")
-# # grdbbox
-# 
-# # Get coordinates from a single observation
-# data.lat <- data$lat[27]
-# data.lon <- data$lon[27]
-# 
-# ## Define image as raster
-# grdr <- raster(grd)
-# 
-# ## Input the points x (longitude), then y (latitude)
-# point_to_sample <- c(data.lon, data.lat)
-# xycoords <- adf(matrix(data = point_to_sample, nrow = 1, ncol = 2))
-# names(xycoords) <- c("x", "y")
-# 
-# xy <- SpatialPoints(coords=xycoords, proj4string=grdproj)
-# 
-# ## Extract single pixel from raster image
-# pixelval <- extract(grdr, xy, buffer = NULL)
-# 
-# # Have to convert to 8-bit binary string, and reverse to get the count correct
-# # (also reverse the 2-bit strings in the MODIS Cloud Mask table)
-# pixelval <- rev(t(digitsBase(pixelval, base= 2, 8)))
-# 
-# print(pixelval)
-# 
-# ## Extract cloud indicator
-# cloud_indicator <- pixelval[2:3]
-# print(cloud_indicator)
-# 
-# ## Cloud Indicator from MOD35 metadata
-# # Unobstructed FOV Quality Flag
-# # 00 = Cloudy                   
-# # 01 = Uncertain                
-# # 10 = Probably  Clear          
-# # 11 = Confident  Clear 
-################################################################################
-### Get Date from MODIS files ##############################################
+## MYD35
+registerDoParallel(cl <- makeCluster(4))
 
-tiffns <- list.files(path.tif.cloudmask, pattern=".tif", full.names=TRUE)
-tiffns
-
-mod.dates.df <- dates_from_fileslist(tiffns)
-mod.dates.df
-
-################################################################################
-### Download MODIS MOD02 files #################################################
-
-# # install MODIS R-package
-# install.packages("MODIS", repos="http://R-Forge.R-project.org")
-# 
-# # Load library
-# library(MODIS)
-# 
-?getHdf
-# getProduct() 
-
+foreach(i in fls.myd35), .packages = lib) %dopar% {
+  
+  if (as.numeric(substr(basename(fls.myd35[i]),19,22)) > 1700)
+    file.rename(from = fls.myd35[i],
+                to = paste0(path.hdf.sub, basename(fls.myd35[i])))
+  
+  stopCluster(cl)

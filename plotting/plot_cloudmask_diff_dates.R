b6b8bbc212dc05b1fb5ba29e04cf2ea448b39784
schmingo <sschlauss@gmail.com>
2014-01-17 12:56:02 +0100

    init

---
+++ plotting/plot_cloudmask_diff_dates.R
@@ -0,0 +1,30 @@
+################################################################################
+## BiFoRe Scripts                                                             ##
+##                                                                            ##
+## PLOT CLOUDMASK DIFF DATES                                                  ##
+##                                                                            ##
+## Author: Simon Schlauss (sschlauss@gmail.com)                               ##
+## Version: 2014-01-06                                                        ##
+##                                                                            ##
+################################################################################
+
+## Clear workspace
+rm(list = ls(all = TRUE))
+
+## Required libraries
+lib <- c(ggplot2)
+
+lapply(lib, function(...) require(..., character.only = TRUE))
+
+## Set working directory
+setwd("/home/schmingo/Dropbox/Diplomarbeit/code/bifore/src/")
+
+path.nocloud.csv <- ("csv/kili/cloudcheck_diff_dates.csv")
+
+################################################################################
+### Set filepaths ##############################################################
+
+data <- read.csv2(path.nocloud.csv,
+                  dec = ".",
+                  header = TRUE, 
+                  stringsAsFactors = TRUE)

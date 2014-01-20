e72e13945963258528612049c855c5e6fc0eedf7
schmingo <sschlauss@gmail.com>
2014-01-20 16:53:37 +0100

    update date conversion

--- preprocessing/unused/modis_extract_calulated_cloud_dates.R
+++ preprocessing/unused/modis_extract_calulated_cloud_dates.R
@@ -13,7 +13,7 @@ rm(list = ls(all = TRUE))
 
 ## Required libraries
 # install.packages("MODIS", repos="http://R-Forge.R-project.org")
-lib <- c("modiscloud", "devtools", "doParallel", "rgdal", "raster", "ggplot2")
+lib <- c()
 lapply(lib, function(...) require(..., character.only = TRUE))
 
 ## Set working directory
@@ -24,6 +24,7 @@ setwd("d:/Dropbox/Diplomarbeit/code/bifore/src/")
 ### Set filepaths ##############################################################
 
 path.nocloud.csv <- ("csv/kili/biodiversity_data_cloudchecked.csv")
+path.nocloud.out.csv <- ("csv/kili/biodiversity_data_cloudchecked_sub.csv")
 
 ################################################################################
 ### Import biodiversity dataset ################################################
@@ -31,7 +32,26 @@ path.nocloud.csv <- ("csv/kili/biodiversity_data_cloudchecked.csv")
 data <- read.csv2(path.nocloud.csv,
 #                   dec = ".",
                   header = TRUE, 
-                  stringsAsFactors = TRUE)
-
+                  stringsAsFactors = FALSE)
 
 data.cloud <- data[c(2,4,3)]
+
+str(data.cloud)
+
+data.cloud <- transform(data.cloud, date_nocloud = paste0(substr(data.cloud$date_nocloud, 1,4), 
+                                                   "-", 
+                                                   substr(data.cloud$date_nocloud, 5, 7), 
+                                                   "_", 
+                                                   substr(data.cloud$date_nocloud, 9, 12)))
+
+data.cloud <- transform(data.cloud, date_observation = paste0(substr(data.cloud$date_observation, 1,4), 
+                                                              "-", 
+                                                              substr(data.cloud$date_observation, 5, 7)))
+
+write.table(data.cloud, 
+            file = path.nocloud.out.csv,
+            dec = ".",
+            quote = FALSE,
+            col.names = TRUE,
+            row.names = FALSE,
+            sep = ";")

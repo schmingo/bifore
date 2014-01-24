c2fda13d5bf344d724a75bc19aaa6b16d4ccd130
schmingo <sschlauss@gmail.com>
2014-01-24 18:15:47 +0100

    add filepaths, load biodiversity .csv

--- preprocessing/lvl0300_modis_extract_myd02_greyvalues.R
+++ preprocessing/lvl0300_modis_extract_myd02_greyvalues.R
@@ -5,7 +5,7 @@
 ##                                                                            ##
 ##                                                                            ##
 ## Author: Simon Schlauss (sschlauss@gmail.com)                               ##
-## Version: 201$-01-24                                                        ##
+## Version: 2014-01-24                                                        ##
 ##                                                                            ##
 ################################################################################
 
@@ -16,3 +16,26 @@ rm(list = ls(all = TRUE))
 ## Required libraries
 lib <- c("rgdal", "doParallel", "raster", "matrixStats")
 lapply(lib, function(...) require(..., character.only = TRUE))
+
+## Set working directory
+setwd("/home/schmingo/Dropbox/Diplomarbeit/code/bifore/src/")
+
+################################################################################
+### Set filepaths ##############################################################
+
+path.hdf <- "/home/schmingo/SAVE/Diplomarbeit/myd02_hdf/"
+path.tif <- "/home/schmingo/SAVE/Diplomarbeit/myd02_tif/"
+
+path.biodiversity.csv <- "csv/kili/biodiversity_data_subset.csv"
+
+
+################################################################################
+### Import biodiversity dataset ################################################
+
+data.bio <- read.csv2(path.biodiversity.csv,
+                      dec = ".",
+                      header = TRUE, 
+                      stringsAsFactors = TRUE)
+
+################################################################################
+### 

d81f83eedeaf539615750eef42a90d5af1f925e4
schmingo <sschlauss@gmail.com>
2014-02-12 17:48:04 +0100

    remove unused stuff

--- random_forest/lvl0500_random_forest_modis.R
+++ random_forest/lvl0500_random_forest_modis.R
@@ -21,15 +21,11 @@ lapply(lib, function(...) require(..., character.only = TRUE))
 setwd("D:/Dropbox/Diplomarbeit/code/bifore/src/")
 
 ## Import dataset
-data.t <- read.csv2("csv/kili/lvl0300_biodiversity_data_t.csv",
-                    dec = ".",
+train.data <- read.csv2("csv/kili/lvl0400_speciesNR.csv",
+                    dec = ",",
                     header = TRUE,
                     stringsAsFactors = FALSE)
 
-data <- read.csv2("csv/kili/lvl0300_biodiversity_data.csv",
-                  dec = ".",
-                  header = TRUE,
-                  stringsAsFactors = FALSE)
 
 
 ################################################################################
@@ -84,46 +80,46 @@ data <- read.csv2("csv/kili/lvl0300_biodiversity_data.csv",
 # detach(data)
 # names(tmp.data)
 
-## Combine MODIS and abundance data 
-train.data <- cbind(data.greyval, tmp.speciesnr)
-names(train.data)
-
-
-## Deal with NA values using rfImpute function
-?rfImpute
-
-n.tree <- 500 # Number of trees to grow
-m.try <- 7 # Number of variables randomly sampled as candidates at each split
-
-train.rf.imp <- rfImpute(x = train.data[,3:ncol(train.data)-1],
-                         y = train.data[,names(train.data) %in% c("SpeciesNr")],
-                         ntree = 500,
-                         iter = 5,
-                         mtry = 7,
-                         do.trace = 100)
-
-print(train.rf.imp)
-# see http://stackoverflow.com/questions/8370455/how-to-use-random-forests-in-r-with-missing-values
+# ## Combine MODIS and abundance data 
+# train.data <- cbind(data.greyval, tmp.speciesnr)
+# names(train.data)
+# 
+# 
+# ## Deal with NA values using rfImpute function
+# ?rfImpute
+# 
+# n.tree <- 500 # Number of trees to grow
+# m.try <- 7 # Number of variables randomly sampled as candidates at each split
+# 
+# train.rf.imp <- rfImpute(x = train.data[,3:ncol(train.data)-1],
+#                          y = train.data[,names(train.data) %in% c("SpeciesNr")],
+#                          ntree = 500,
+#                          iter = 5,
+#                          mtry = 7,
+#                          do.trace = 100)
+# 
+# print(train.rf.imp)
+# # see http://stackoverflow.com/questions/8370455/how-to-use-random-forests-in-r-with-missing-values
 
 ################################################################################
 ### Random Forest ##############################################################
 
 # ## Define desired parameters
-# n.tree <- 500 # Number of trees to grow
-# m.try <- 7 # Number of variables randomly sampled as candidates at each split
+n.tree <- 500 # Number of trees to grow
+m.try <- 7 # Number of variables randomly sampled as candidates at each split
 # 
 # 
 # ## Check colnames to get the right predictor values
-# names(train.data[,3:ncol(train.data)-1])
-# ## Function 
-# train.rf <- randomForest(train.data[,3:ncol(train.data)-1],
-#                          train.data[,names(train.data) %in% c("SpeciesNr")],
-#                          importance = TRUE,
-# #                          na.action = na.omit(train.data),
-# #                          type="classification",
-#                          do.trace = 100)
-# 
-# print(train.rf)
+names(train.data[,5:ncol(train.data)-1])
+## Function 
+train.rf <- randomForest(train.data[,5:ncol(train.data)-1],
+                         train.data[,names(train.data) %in% c("SpeciesNr")],
+                         importance = TRUE,
+                         na.action = na.omit(train.data),
+#                          type="classification",
+                         do.trace = 100)
+
+print(train.rf)
 
 
 ################################################################################

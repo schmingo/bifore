15cb03c115d1c2342b3e3be5fb6ef5c153722194
schmingo <sschlauss@gmail.com>
2014-03-01 13:50:31 +0100

    update version#

--- random_forest/lvl0500_random_forest_single.species.R
+++ random_forest/lvl0500_random_forest_single.species.R
@@ -4,7 +4,7 @@
 ## RANDOM FOREST FOR MODIS DATA                                               ##
 ##                                                                            ##
 ## Author: Simon Schlauss (sschlauss@gmail.com)                               ##
-## Version: 2014-02-25                                                        ##
+## Version: 2014-03-01                                                        ##
 ##                                                                            ##
 ################################################################################
 

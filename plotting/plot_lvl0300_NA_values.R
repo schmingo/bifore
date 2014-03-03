8d6a10fe4aaa9581c065a2bc8710dfad5ce9c601
schmingo <sschlauss@gmail.com>
2014-03-03 13:56:46 +0100

    add ggplots

--- plotting/plot_lvl0300_NA_values.R
+++ plotting/plot_lvl0300_NA_values.R
@@ -45,11 +45,14 @@ df.diff.all <- data.raw[107:144]
 df.sd.all <- data.raw[145:182]
 
 ## Create df, count NA's for each MODIS band
-df.NA <- cbind(colSums(is.na(df.greyval.all)),
-               colSums(is.na(df.diff.all)),
-               colSums(is.na(df.sd.all)))
+df.NA <- cbind.data.frame(colSums(is.na(df.greyval.all)),
+                          colSums(is.na(df.diff.all)),
+                          colSums(is.na(df.sd.all)))
 
 names(df.NA) <- c("NA_greyvalues", "NA_diff", "NA_sd")
+df.NA$bandnames <- bandnames[,1]
+
+df.NA.melt <- melt(df.NA, id.vars="bandnames")
 
 
 ################################################################################
@@ -58,10 +61,11 @@ names(df.NA) <- c("NA_greyvalues", "NA_diff", "NA_sd")
 
 ## Plot single subset (greyvalues, diff or sd)
 qplot(y=df.NA[,1], 
-      x=row.names(df.NA), 
+      x=df.NA$bandnames, 
       geom = "bar", 
       binwidth = 2, 
-      stat="identity")
+      stat="identity",
+      position="dodge")
 
 
 # ## Using barplot()
@@ -72,3 +76,10 @@ qplot(y=df.NA[,1],
 #         xlab = "MODIS bands")
 
 
+ggplot(df.NA.melt, aes(df.NA.melt$variable, df.NA.melt$value, fill = df.NA.melt$bandnames)) +
+  geom_bar(stat= "identity", position="dodge") + 
+  theme(axis.text.x=element_text(angle=-90))
+
+ggplot(df.NA.melt, aes(x=df.NA.melt$bandnames, y=df.NA.melt$value, fill=df.NA.melt$variable)) + 
+  geom_bar(position="dodge", stat="identity") + 
+  theme(axis.text.x=element_text(angle=-90))

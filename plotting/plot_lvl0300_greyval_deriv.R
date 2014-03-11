bb28583916630660c1219a751b5dd6c794074017
schmingo <sschlauss@gmail.com>
2014-03-11 12:57:47 +0100

    add greyvalues emissive and greyvalues reflective

--- plotting/plot_lvl0300_greyval_deriv.R
+++ plotting/plot_lvl0300_greyval_deriv.R
@@ -1,11 +1,11 @@
 ################################################################################
 ## BiFoRe Scripts                                                             ##
 ##                                                                            ##
-## PLOT EXTRACT GREYVALUES, FIRST DERIVATE & CALCULATED SD                    ##
+## PLOT EXTRACT GREYVALUES, FIRST DERIVATE                                    ##
 ##                                                                            ##
 ##                                                                            ##
 ## Author: Simon Schlauss (sschlauss@gmail.com)                               ##
-## Version: 2014-03-10                                                        ##
+## Version: 2014-03-11                                                        ##
 ##                                                                            ##
 ################################################################################
 
@@ -34,12 +34,36 @@ data.raw <- read.csv2(path.lvl0300.csv,
 
 bandnames <- read.csv2(path.bandnames.csv,
                        header = TRUE,
-                       stringsAsFactors = TRUE)
+                       stringsAsFactors = FALSE)
 
 ################################################################################
 ### Subsetting #################################################################
 ################################################################################
 
+## greyvalues reflective bands 
+data.greyval.reflective <- cbind.data.frame(data.raw[,3],
+                                            data.raw[,69:89],
+                                            data.raw[,96])
+names(data.greyval.reflective) <- c("date_nocloud",
+                                    as.character(bandnames[1:21,2]),
+                                    as.character(bandnames[28,2]))
+names(data.greyval.reflective)
+
+greyval.reflective.melt <- melt(data.greyval.reflective, id = "date_nocloud", 
+                                measured = c(data.greyval.reflective[,2:ncol(data.greyval.reflective)]))
+
+
+## greyvalues emissive bands
+data.greyval.emissive <- cbind.data.frame(data.raw[,3],
+                                          data.raw[,90:95],
+                                          data.raw[,97:106])
+names(data.greyval.emissive) <- c("date_nocloud",
+                                  as.character(bandnames[22:27,2]),
+                                  as.character(bandnames[29:38,2]))
+names(data.greyval.emissive)
+
+greyval.emissive.melt <- melt(data.greyval.emissive, id = "date_nocloud", 
+                              measured = c(data.greyval.emissive[,2:ncol(data.greyval.emissive)]))
 
 
 
@@ -47,3 +71,12 @@ bandnames <- read.csv2(path.bandnames.csv,
 ### Plotting ###################################################################
 ################################################################################
 
+plot.greyval.reflective <- ggplot(greyval.reflective.melt, aes(x = variable, y = value)) +
+  geom_boxplot()
+plot.greyval.reflective
+
+
+
+plot.greyval.emissive <- ggplot(greyval.emissive.melt, aes(x = variable, y = value)) +
+  geom_boxplot()
+plot.greyval.emissive

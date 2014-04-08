ac6133c5bbb7b56e7af10de1c9a2c3e64c82bcf1
schmingo <sschlauss@gmail.com>
2014-04-08 14:28:42 +0200

    rename output files

--- random_forest/lvl0400_prepare_dataset_strat_sampling.R
+++ random_forest/lvl0400_prepare_dataset_strat_sampling.R
@@ -34,8 +34,8 @@ file.out.braunblanq.all <- "csv/kili/lvl0400_rf_strat_braun-blanquet_all.csv"
 file.out.prevalence.all <- "csv/kili/lvl0400_rf_strat_prevalence_all.csv"
 # file.out.prevalence.10 <- "csv/kili/lvl0400_rf_strat_prevalence_10.csv"
 
-# file.out.simple.noofspecies.all <- "csv/kili/lvl0400_strat_simple_number.of.species_all.csv"
-# file.out.simple.noofspecies.10 <- "csv/kili/lvl0400_strat_simple_number.of.species_10.csv"
+# file.out.simple.noofspecies.all <- "csv/kili/lvl0400_strat_simple_number-of-species_all.csv"
+# file.out.simple.noofspecies.10 <- "csv/kili/lvl0400_strat_simple_number-of-species_10.csv"
 
 
 ################################################################################

98e1b35b92b34a7d332547666c8e43a1bc0a927e
schmingo <sschlauss@gmail.com>
2014-04-22 10:24:25 +0200

    loop all species

--- random_forest/lvl0600_rf_classification_prevalence.R
+++ random_forest/lvl0600_rf_classification_prevalence.R
@@ -15,12 +15,17 @@ cat("\014")
 rm(list = ls(all = TRUE))
 
 ## Required libraries
-lib <- c("randomForest", "foreach")
+lib <- c("randomForest", "foreach", "doParallel")
 lapply(lib, function(...) require(..., character.only = TRUE))
+
+ncores <- detectCores()-1
 
 ## set working directory
 # setwd("/home/schmingo/Dropbox/Diplomarbeit/code/bifore/src/")
 setwd("D:/Dropbox/Diplomarbeit/code/bifore/src/")
+
+## Timekeeping
+starttime <- Sys.time()
 
 
 ################################################################################
@@ -36,75 +41,100 @@ data.raw <- read.csv2("csv/kili/lvl0400_rf_strat_prevalence_10.csv",
 ################################################################################
 ### Combining data for randomForest ############################################
 ################################################################################
+set.seed(50)
 
 ## List species
 lst.species <- names(data.raw[13:67])
 lst.species
-
-species <- lst.species[47]
-
 
 ## Split incoming dataset
 df.greyval <- data.raw[68:97]  ## greyvalues
 df.diff <- data.raw[98:125]  ## diff
 df.sd <- data.raw[126:155]  ## sd 
 
-## Select species data
-df.species <- data.frame(data.raw[,names(data.raw) %in% c(species)])
-names(df.species) <- species
+## Calculate randomForest for all Species
+registerDoParallel(cl <- makeCluster(ncores))
 
-tmp.species <- df.species
+species.conf.matrix <- foreach(s = lst.species, 
+                               .combine = "cbind", 
+                               .packages = lib) %dopar% {
 
-# summary(tmp.species)
-
-## Create multiple dataframes with single species as predictor datasets
-df.spec.greyval <- cbind(df.greyval, tmp.species)
-# df.spec.diff <- cbind(df.diff, tmp.species)
-# df.spec.greyval.diff <- cbind(df.greyval, df.diff, tmp.species)
-# df.spec.greyval.sd <- cbind(df.greyval, df.sd, tmp.species)
-# df.spec.diff.sd <- cbind(df.diff, df.sd, tmp.species)
-
-
-## Define Random Forest input data #############################################
-train.data <- df.spec.greyval ## Insert input dataset here!
-
-
-################################################################################
-### Random Forest function #####################################################
-### Classification - single species ############################################
-################################################################################
-
-predictor_modisVAL <- train.data[,1:ncol(train.data)-1]
-response_speciesCLASS <- as.factor(train.data[,ncol(train.data)])
-
-## Initiate rF variables
-conf.1.1 = NULL
-conf.1.2 = NULL
-conf.2.1 = NULL
-conf.2.2 = NULL
-
-## Function ####################################################################
-set.seed(50)
-
-foreach(i = seq(1:100)) %do% {
+#   species <- lst.species[47]
   
-  train.rf <- randomForest(x = predictor_modisVAL,
-                           y = response_speciesCLASS,
-                           importance = TRUE,
-                           ntree = 500,
-                           mtry = 2,
-                           nodesize = 2,
-                           type="classification",
-                           do.trace = FALSE)
+  ## Select species data
+  df.species <- data.frame(data.raw[,names(data.raw) %in% c(s)])
+  names(df.species) <- s
   
-  conf.1.1[i] = train.rf$confusion[1,1]
-  conf.1.2[i] = train.rf$confusion[1,2]
-  conf.2.1[i] = train.rf$confusion[2,1]
-  conf.2.2[i] = train.rf$confusion[2,2]
+  tmp.species <- df.species
+  
+  # summary(tmp.species)
+  
+  ## Create dataframe with single species as predictor dataset
+  df.spec.greyval <- cbind(df.greyval, tmp.species)
+  
+  
+  
+  ## Define Random Forest input data ###########################################
+  train.data <- df.spec.greyval
+  
+  
+  ##############################################################################
+  ### Random Forest function ###################################################
+  ### Classification - single species ##########################################
+  ##############################################################################
+  
+  predictor_modisVAL <- train.data[,1:ncol(train.data)-1]
+  response_speciesCLASS <- as.factor(train.data[,ncol(train.data)])
+  
+  ## Initialize confusion matrix variables
+  conf.1.1 = NULL
+  conf.1.2 = NULL
+  conf.2.1 = NULL
+  conf.2.2 = NULL
+  
+  ## Function ##################################################################
+  
+  foreach(i = seq(1:100)) %do% {
+    
+    train.rf <- randomForest(x = predictor_modisVAL,
+                             y = response_speciesCLASS,
+                             importance = TRUE,
+                             ntree = 500,
+                             mtry = 2,
+                             nodesize = 2,
+                             type="classification",
+                             do.trace = FALSE)
+    
+    conf.1.1[i] = train.rf$confusion[1,1]
+    conf.1.2[i] = train.rf$confusion[1,2]
+    conf.2.1[i] = train.rf$confusion[2,1]
+    conf.2.2[i] = train.rf$confusion[2,2]
+  }
+  
+  ## Get mean values
+  mean(conf.1.1)
+  mean(conf.1.2)
+  mean(conf.2.1)
+  mean(conf.2.2)
+  
+  ## Save mean confusion matrix values into a dataframe
+  conf.matrix <- as.data.frame(c(mean(conf.1.1), 
+                                 mean(conf.1.2), 
+                                 mean(conf.2.1), 
+                                 mean(conf.2.2)))
+  
+  ## Set colnames (species)
+  names(conf.matrix) <- s
+  return(conf.matrix)
+  
 }
 
-## Get mean values
-mean(conf.1.1)
-mean(conf.1.2)
-mean(conf.2.1)
-mean(conf.2.2)
+
+## Close parallel backend
+stopCluster(cl)
+
+
+## Timekeeping
+endtime <- Sys.time()
+time <- endtime - starttime
+time

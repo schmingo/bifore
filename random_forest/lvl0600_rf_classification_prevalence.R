770a237a362347b3676934c54ecd3d17a37b9b20
schmingo <sschlauss@gmail.com>
2014-04-22 12:11:15 +0200

    add rownames to csv

--- random_forest/lvl0600_rf_classification_prevalence.R
+++ random_forest/lvl0600_rf_classification_prevalence.R
@@ -22,7 +22,10 @@ ncores <- detectCores()-1
 
 ## set working directory
 # setwd("/home/schmingo/Dropbox/Diplomarbeit/code/bifore/src/")
-setwd("D:/Dropbox/Diplomarbeit/code/bifore/src/")
+setwd("D:/Dropbox/Diplomarbeit/code/bifore/src/csv/kili/")
+
+## Set filenames
+file.out.confusion.species.10 <- "lvl0600_rf_prevalence_10_mean_confmatrix.csv"
 
 ## Timekeeping
 starttime <- Sys.time()
@@ -32,7 +35,7 @@ starttime <- Sys.time()
 ### Import dataset #############################################################
 ################################################################################
 
-data.raw <- read.csv2("csv/kili/lvl0400_rf_strat_prevalence_10.csv",
+data.raw <- read.csv2("lvl0400_rf_strat_prevalence_10.csv",
                       dec = ",",
                       header = TRUE,
                       stringsAsFactors = FALSE)
@@ -58,92 +61,113 @@ registerDoParallel(cl <- makeCluster(ncores))
 species.conf.matrix <- foreach(s = lst.species, 
                                .combine = "cbind", 
                                .packages = lib) %dopar% {
-
-#   species <- lst.species[47]
-  
-  ## Select species data
-  df.species <- data.frame(data.raw[,names(data.raw) %in% c(s)])
-  names(df.species) <- s
-  
-  tmp.species <- df.species
-  
-  # summary(tmp.species)
-  
-  ## Create dataframe with single species as predictor dataset
-  df.spec.greyval <- cbind(df.greyval, tmp.species)
-  
-  
-  
-  ## Define Random Forest input data ###########################################
-  train.data <- df.spec.greyval
-  
-  
-  ##############################################################################
-  ### Random Forest function ###################################################
-  ### Classification - single species ##########################################
-  ##############################################################################
-  
-  predictor_modisVAL <- train.data[,1:ncol(train.data)-1]
-  response_speciesCLASS <- as.factor(train.data[,ncol(train.data)])
-  
-  ## Initialize confusion matrix variables
-  conf.1.1 = NULL
-  conf.1.2 = NULL
-  conf.1.3 = NULL
-  conf.2.1 = NULL
-  conf.2.2 = NULL
-  conf.2.3 = NULL
-  
-  ## Function ##################################################################
-  
-  foreach(i = seq(1:100)) %do% {
-    
-    train.rf <- randomForest(x = predictor_modisVAL,
-                             y = response_speciesCLASS,
-                             importance = TRUE,
-                             ntree = 500,
-#                              mtry = 5,
-                             nodesize = 2,
-                             type="classification",
-                             do.trace = FALSE)
-    
-    conf.1.1[i] = train.rf$confusion[1,1]
-    conf.1.2[i] = train.rf$confusion[1,2]
-    conf.1.3[i] = train.rf$confusion[1,3]
-    conf.2.1[i] = train.rf$confusion[2,1]
-    conf.2.2[i] = train.rf$confusion[2,2]
-    conf.2.3[i] = train.rf$confusion[2,3]
-
-  }
-  
-  ## Get mean values
-  mean(conf.1.1)
-  mean(conf.1.2)
-  mean(conf.1.3)
-  mean(conf.2.1)
-  mean(conf.2.2)
-  mean(conf.2.3)
-  
-  ## Save mean confusion matrix values into a dataframe
-  conf.matrix <- as.data.frame(c(mean(conf.1.1), 
-                                 mean(conf.1.2),
-                                 mean(conf.1.3),
-                                 mean(conf.2.1), 
-                                 mean(conf.2.2),
-                                 mean(conf.2.3)))
-  
-  ## Set colnames (species)
-  names(conf.matrix) <- s
-  return(conf.matrix)
-  
-}
+                                 
+                                 #   species <- lst.species[47]
+                                 
+                                 ## Select species data
+                                 df.species <- data.frame(data.raw[,names(data.raw) %in% c(s)])
+                                 names(df.species) <- s
+                                 
+                                 tmp.species <- df.species
+                                 
+                                 # summary(tmp.species)
+                                 
+                                 ## Create dataframe with single species as predictor dataset
+                                 df.spec.greyval <- cbind(df.greyval, tmp.species)
+                                 
+                                 
+                                 
+                                 ## Define Random Forest input data ###########################################
+                                 train.data <- df.spec.greyval
+                                 
+                                 
+                                 ##############################################################################
+                                 ### Random Forest function ###################################################
+                                 ### Classification - single species ##########################################
+                                 ##############################################################################
+                                 
+                                 predictor_modisVAL <- train.data[,1:ncol(train.data)-1]
+                                 response_speciesCLASS <- as.factor(train.data[,ncol(train.data)])
+                                 
+                                 ## Initialize confusion matrix variables
+                                 conf.1.1 = NULL
+                                 conf.1.2 = NULL
+                                 conf.1.3 = NULL
+                                 conf.2.1 = NULL
+                                 conf.2.2 = NULL
+                                 conf.2.3 = NULL
+                                 
+                                 ## Function ##################################################################
+                                 
+                                 foreach(i = seq(1:100)) %do% {
+                                   
+                                   train.rf <- randomForest(x = predictor_modisVAL,
+                                                            y = response_speciesCLASS,
+                                                            importance = TRUE,
+                                                            ntree = 500,
+                                                            #                              mtry = 5,
+                                                            nodesize = 2,
+                                                            type="classification",
+                                                            do.trace = FALSE)
+                                   
+                                   conf.1.1[i] = train.rf$confusion[1,1]
+                                   conf.1.2[i] = train.rf$confusion[1,2]
+                                   conf.1.3[i] = train.rf$confusion[1,3]
+                                   conf.2.1[i] = train.rf$confusion[2,1]
+                                   conf.2.2[i] = train.rf$confusion[2,2]
+                                   conf.2.3[i] = train.rf$confusion[2,3]
+                                   
+                                 }
+                                 
+                                 ## Get mean values
+                                 mean(conf.1.1)
+                                 mean(conf.1.2)
+                                 mean(conf.1.3)
+                                 mean(conf.2.1)
+                                 mean(conf.2.2)
+                                 mean(conf.2.3)
+                                 
+                                 ## Save mean confusion matrix values into a dataframe
+                                 conf.matrix <- as.data.frame(c(mean(conf.1.1), 
+                                                                mean(conf.1.2),
+                                                                mean(conf.1.3),
+                                                                mean(conf.2.1), 
+                                                                mean(conf.2.2),
+                                                                mean(conf.2.3)))
+                                 
+                                 ## Set colnames (species)
+                                 names(conf.matrix) <- s
+                                 return(conf.matrix)
+                                 
+                               }
 
 
 ## Close parallel backend
 stopCluster(cl)
 
 
+## Set rownames for new dataframe
+df.rownames <- as.data.frame(c("Actual=0, predict=0",
+                               "Actual=0, predict=1",
+                               "Class.error 0",
+                               "Actual=1, predict=0",
+                               "Actual=1, predict=1",
+                               "Class.error 1"))
+names(df.rownames) <- "confusion.matrix"
+
+species.conf.matrix <- cbind(df.rownames,
+                             species.conf.matrix)
+
 ## Timekeeping
 endtime <- Sys.time()
 time <- endtime - starttime
 time
+
+## write confusion matrix table
+write.table(species.conf.matrix, 
+            file = file.out.confusion.species.10,
+            dec = ",",
+            quote = FALSE,
+            col.names = TRUE,
+            row.names = FALSE,
+            sep = ";")
--- random_forest/lvl0600_rf_classification_prevalence_testing.R
+++ random_forest/lvl0600_rf_classification_prevalence_testing.R
@@ -22,7 +22,7 @@ ncores <- detectCores()-1
 
 ## set working directory
 # setwd("/home/schmingo/Dropbox/Diplomarbeit/code/bifore/src/")
-setwd("D:/Dropbox/Diplomarbeit/code/bifore/src/")
+setwd("D:/Dropbox/Diplomarbeit/code/bifore/src/csv/kili/")
 
 ## Timekeeping
 starttime <- Sys.time()
@@ -32,7 +32,7 @@ starttime <- Sys.time()
 ### Import dataset #############################################################
 ################################################################################
 
-data.raw <- read.csv2("csv/kili/lvl0400_rf_strat_prevalence_10.csv",
+data.raw <- read.csv2("lvl0400_rf_strat_prevalence_10.csv",
                       dec = ",",
                       header = TRUE,
                       stringsAsFactors = FALSE)
@@ -94,7 +94,7 @@ train.rf <- randomForest(x = predictor_modisVAL,
                          y = response_speciesCLASS,
                          importance = TRUE,
                          ntree = 500,
-#                          mtry = 5,
+                         #                          mtry = 5,
                          nodesize = 2,
                          type="classification",
                          do.trace = FALSE)
@@ -122,3 +122,25 @@ names(conf.matrix) <- s
 conf.matrix
 
 train.rf$confusion    
+
+df.rownames <- as.data.frame(c("Actual=0, predict=0",
+                               "Actual=0, predict=1",
+                               "Class.error 0",
+                               "Actual=1, predict=0",
+                               "Actual=1, predict=1",
+                               "Class.error 1"))
+names(df.rownames) <- "confusion.matrix"
+
+## Set rownames for new dataframe
+conf.matrix <- cbind(df.rownames,
+                     conf.matrix)
+
+file.out.confusion.species.10 <- "lvl0600_rf_prevalence_10_mean_confmatrix_test.csv"
+
+write.table(conf.matrix, 
+            file = file.out.confusion.species.10,
+            dec = ",",
+            quote = FALSE,
+            col.names = TRUE,
+            row.names = FALSE,
+            sep = ";")

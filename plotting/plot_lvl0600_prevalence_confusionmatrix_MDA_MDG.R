494dee155da8378826949eefe24bb2bead572be2
schmingo <sschlauss@gmail.com>
2014-04-28 16:21:55 +0200

    update input data

--- plotting/plot_lvl0600_prevalence_confusionmatrix_MDA_MDG.R
+++ plotting/plot_lvl0600_prevalence_confusionmatrix_MDA_MDG.R
@@ -25,31 +25,33 @@ setwd("D:/Dropbox/Diplomarbeit/code/bifore/src/")
 ### Import dataset #############################################################
 ################################################################################
 
-data.raw.all <- read.csv2("csv/kili/lvl0600_rf_prevalence_species10_mean100.csv",
-                          dec = ",",
-                          header = TRUE,
-                          stringsAsFactors = FALSE)
-
+data.raw <- read.csv2("csv/kili/lvl0600_rf_prevalence_species10_mean100.csv",
+                      dec = ",",
+                      header = TRUE,
+                      row.names = 1,
+                      stringsAsFactors = FALSE)
 
 ################################################################################
-### Plotting - prevalence - all species ########################################
+### Plotting - confusion matrix ################################################
 ################################################################################
 
 ## Define output image | open image port
-png("images/lvl0400_prevalence_all_species.png", 
+png("images/lvl0600_prevalence_confusionmatrix.png", 
     width = 2048 * 6, 
     height = 748 * 6, 
     units = "px", 
     res = 600)
 
-plot <- ggplot(data.raw.all, aes(x=species, y=number.of.species)) + 
-  geom_bar(stat="identity") +
-  scale_fill_grey() +
-  xlab("species") +
-  ylab("prevalence") +
-  ggtitle("Orthoptera prevalence Mt. Kilimanjaro 2002-2012") +
-  theme(axis.text.x = element_text(angle = 270, hjust = 0, vjust = .5, size = 8),
-        plot.title = element_text(lineheight = .8, size = 20))
+plot <- ggplot(df.NA.melt, aes(x=MODIS_bands, y=NA_count, fill=NA_values)) + 
+  geom_bar(position="dodge", stat="identity", width=1, colour="white") +
+  scale_fill_grey(name = "NA values") +
+  #   coord_flip() +
+  xlab("MODIS bands") +
+  ylab("NA counts") +
+  ggtitle("Summary of NA values for MODIS MYD02") +
+  theme(axis.text.x=element_text(angle=90, hjust = 0, vjust = .5),
+        plot.title = element_text(lineheight = .8, size = 20),
+        legend.position=c(.9, .5))
 
 plot
 

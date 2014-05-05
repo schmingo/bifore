2b91b0650ec5712612166833deefd5f02fdf5353
schmingo <sschlauss@gmail.com>
2014-05-05 12:04:04 +0200

    add plot by species / plot by band

--- plotting/plot_lvl0600_prevalence_MDA_MDG.R
+++ plotting/plot_lvl0600_prevalence_MDA_MDG.R
@@ -80,61 +80,59 @@ df.varimp.MDG.emit.melt <- melt(df.varimp.MDG.emit, id="species")
 
 
 ################################################################################
-### Plotting - Mean Decrease Accuracy ##########################################
+### Plotting - Mean Decrease Accuracy - by bands ###############################
 ################################################################################
 
 ## Define output image | open image port
-png("images/lvl0600_prevalence_MeanDecreaseAccuracy_reflective.png", 
-    width = 1024 * 6, 
-    height = 748 * 6, 
-    units = "px", 
-    res = 600)
-
-ggplot(data=df.varimp.MDA.reflect.melt,
-       aes(x=species, y=value, group=variable)) +
-  geom_line() +
-  ylim(-2,15) +
-  xlab(NULL) +
-  ylab("Mean Decrease Accuracy") +
-  theme_bw() +
-  ggtitle("Prevalence - RandomForest - Mean Decrease Accuracy (reflective bands)") +
-  theme(axis.text.x = element_text(angle = 270, hjust = 0, vjust = .5, size = 8),
-        plot.title = element_text(lineheight = .8, size = 20)) +
-  facet_wrap(~variable, as.table=FALSE, ncol = 4)
-
-## Close image port
-graphics.off()
-
-################################################################################
-
-## Define output image | open image port
-png("images/lvl0600_prevalence_MeanDecreaseAccuracy_emissive.png", 
-    width = 1024 * 6, 
-    height = 748 * 6, 
-    units = "px", 
-    res = 600)
-
-ggplot(data=df.varimp.MDA.emit.melt,
-       aes(x=species, y=value, group=variable)) +
-  geom_line() +
-  ylim(-2,15) +
-  xlab(NULL) +
-  ylab("Mean Decrease Accuracy") +
-  theme_bw() +
-  ggtitle("Prevalence - RandomForest - Mean Decrease Accuracy (emissive bands)") +
-  theme(axis.text.x = element_text(angle = 270, hjust = 0, vjust = .5, size = 8),
-        plot.title = element_text(lineheight = .8, size = 20)) +
-  facet_wrap(~variable, as.table=FALSE, ncol = 4)
-
-
-
-## Close image port
-graphics.off()
+# png("images/lvl0600_prevalence_bybands_MDA_reflective.png", 
+#     width = 1024 * 6, 
+#     height = 748 * 6, 
+#     units = "px", 
+#     res = 600)
+# 
+# ggplot(data=df.varimp.MDA.reflect.melt,
+#        aes(x=species, y=value, group=variable)) +
+#   geom_line() +
+#   ylim(-2,15) +
+#   xlab(NULL) +
+#   ylab("Mean Decrease Accuracy") +
+#   theme_bw() +
+#   ggtitle("Prevalence - RandomForest - Mean Decrease Accuracy (reflective bands)") +
+#   theme(axis.text.x = element_text(angle = 270, hjust = 0, vjust = .5, size = 8),
+#         plot.title = element_text(lineheight = .8, size = 20)) +
+#   facet_wrap(~variable, as.table=FALSE, ncol = 4)
+# 
+# ## Close image port
+# graphics.off()
+# 
+# ################################################################################
+# 
+# ## Define output image | open image port
+# png("images/lvl0600_prevalence_bybands_MDA_emissive.png", 
+#     width = 1024 * 6, 
+#     height = 748 * 6, 
+#     units = "px", 
+#     res = 600)
+# 
+# ggplot(data=df.varimp.MDA.emit.melt,
+#        aes(x=species, y=value, group=variable)) +
+#   geom_line() +
+#   ylim(-2,15) +
+#   xlab(NULL) +
+#   ylab("Mean Decrease Accuracy") +
+#   theme_bw() +
+#   ggtitle("Prevalence - RandomForest - Mean Decrease Accuracy (emissive bands)") +
+#   theme(axis.text.x = element_text(angle = 270, hjust = 0, vjust = .5, size = 8),
+#         plot.title = element_text(lineheight = .8, size = 20)) +
+#   facet_wrap(~variable, as.table=FALSE, ncol = 4)
+# 
+# ## Close image port
+# graphics.off()
 
 ################################################################################
 
 ## Define output image | open image port
-png("images/lvl0600_prevalence_MeanDecreaseAccuracy.png", 
+png("images/lvl0600_prevalence_bybands_MDA.png", 
     width = 748 * 6, 
     height = 1024 * 6, 
     units = "px", 
@@ -143,77 +141,73 @@ png("images/lvl0600_prevalence_MeanDecreaseAccuracy.png",
 ggplot(data=df.varimp.MDA.melt,
        aes(x=species, y=value, group=variable)) +
   geom_line() +
-  ylim(-2,15) +
+  ylim(-1,13) +
   xlab(NULL) +
   ylab("Mean Decrease Accuracy") +
   theme_bw() +
   ggtitle("Prevalence - RandomForest - Mean Decrease Accuracy") +
   theme(axis.text.x = element_text(angle = 270, hjust = 0, vjust = .5, size = 8),
         plot.title = element_text(lineheight = .8, size = 20)) +
-  facet_wrap(~variable, as.table=FALSE, ncol = 4)
-
-
+  facet_wrap(~variable, as.table=TRUE, ncol = 3)
 
 ## Close image port
 graphics.off()
 
 
 ################################################################################
-### Plotting - Mean Decrease Gini ##############################################
+### Plotting - Mean Decrease Gini - by bands ###################################
 ################################################################################
 
 ## Define output image | open image port
-png("images/lvl0600_prevalence_MeanDecreaseGini_reflective.png", 
-    width = 1024 * 6, 
-    height = 748 * 6, 
-    units = "px", 
-    res = 600)
-
-ggplot(data=df.varimp.MDG.reflect.melt,
-       aes(x=species, y=value, group=variable)) +
-  geom_line() +
-  ylim(-2,15) +
-  xlab(NULL) +
-  ylab("Mean Decrease Gini") +
-  theme_bw() +
-  ggtitle("Prevalence - RandomForest - Mean Decrease Gini (reflective bands)") +
-  theme(axis.text.x = element_text(angle = 270, hjust = 0, vjust = .5, size = 8),
-        plot.title = element_text(lineheight = .8, size = 20)) +
-  facet_wrap(~variable, as.table=FALSE, ncol = 4)
-
-## Close image port
-graphics.off()
-
-################################################################################
-
-## Define output image | open image port
-png("images/lvl0600_prevalence_MeanDecreaseGini_emissive.png", 
-    width = 1024 * 6, 
-    height = 748 * 6, 
-    units = "px", 
-    res = 600)
-
-ggplot(data=df.varimp.MDG.emit.melt,
-       aes(x=species, y=value, group=variable)) +
-  geom_line() +
-  ylim(-2,15) +
-  xlab(NULL) +
-  ylab("Mean Decrease Gini") +
-  theme_bw() +
-  ggtitle("Prevalence - RandomForest - Mean Decrease Gini (emissive bands)") +
-  theme(axis.text.x = element_text(angle = 270, hjust = 0, vjust = .5, size = 8),
-        plot.title = element_text(lineheight = .8, size = 20)) +
-  facet_wrap(~variable, as.table=FALSE, ncol = 4)
-
-
-
-## Close image port
-graphics.off()
+# png("images/lvl0600_prevalence_bybands_MDG_reflective.png", 
+#     width = 1024 * 6, 
+#     height = 748 * 6, 
+#     units = "px", 
+#     res = 600)
+# 
+# ggplot(data=df.varimp.MDG.reflect.melt,
+#        aes(x=species, y=value, group=variable)) +
+#   geom_line() +
+#   ylim(-2,15) +
+#   xlab(NULL) +
+#   ylab("Mean Decrease Gini") +
+#   theme_bw() +
+#   ggtitle("Prevalence - RandomForest - Mean Decrease Gini (reflective bands)") +
+#   theme(axis.text.x = element_text(angle = 270, hjust = 0, vjust = .5, size = 8),
+#         plot.title = element_text(lineheight = .8, size = 20)) +
+#   facet_wrap(~variable, as.table=FALSE, ncol = 4)
+# 
+# ## Close image port
+# graphics.off()
+# 
+# ################################################################################
+# 
+# ## Define output image | open image port
+# png("images/lvl0600_prevalence_bybands_MDG_bybands.png", 
+#     width = 1024 * 6, 
+#     height = 748 * 6, 
+#     units = "px", 
+#     res = 600)
+# 
+# ggplot(data=df.varimp.MDG.emit.melt,
+#        aes(x=species, y=value, group=variable)) +
+#   geom_line() +
+#   ylim(-2,15) +
+#   xlab(NULL) +
+#   ylab("Mean Decrease Gini") +
+#   theme_bw() +
+#   ggtitle("Prevalence - RandomForest - Mean Decrease Gini (emissive bands)") +
+#   theme(axis.text.x = element_text(angle = 270, hjust = 0, vjust = .5, size = 8),
+#         plot.title = element_text(lineheight = .8, size = 20)) +
+#   facet_wrap(~variable, as.table=FALSE, ncol = 4)
+# 
+# ## Close image port
+# graphics.off()
 
 ################################################################################
 
 ## Define output image | open image port
-png("images/lvl0600_prevalence_MeanDecreaseGini.png", 
+png("images/lvl0600_prevalence_bybands_MDG.png", 
     width = 748 * 6, 
     height = 1024 * 6, 
     units = "px", 
@@ -222,16 +216,72 @@ png("images/lvl0600_prevalence_MeanDecreaseGini.png",
 ggplot(data=df.varimp.MDG.melt,
        aes(x=species, y=value, group=variable)) +
   geom_line() +
-  ylim(-2,15) +
+  ylim(0,5) +
   xlab(NULL) +
   ylab("Mean Decrease Gini") +
   theme_bw() +
   ggtitle("Prevalence - RandomForest - Mean Decrease Gini") +
   theme(axis.text.x = element_text(angle = 270, hjust = 0, vjust = .5, size = 8),
         plot.title = element_text(lineheight = .8, size = 20)) +
-  facet_wrap(~variable, as.table=FALSE, ncol = 4)
+  facet_wrap(~variable, as.table=TRUE, ncol = 3)
 
 
+
+## Close image port
+graphics.off()
+
+
+
+
+################################################################################
+### Plotting - Mean Decrease Accuracy - by species #############################
+################################################################################
+
+## Define output image | open image port
+png("images/lvl0600_prevalence_byspecies_MDA.png", 
+    width = 1024 * 6, 
+    height = 1024 * 6, 
+    units = "px", 
+    res = 600)
+
+ggplot(data=df.varimp.MDA.melt,
+       aes(x=variable, y=value, group=species)) +
+  geom_line() +
+  ylim(-1,13) +
+  xlab("MODIS bands") +
+  ylab("Mean Decrease Accuracy") +
+  theme_bw() +
+  ggtitle("Prevalence - RandomForest - Mean Decrease Accuracy - by species") +
+  theme(axis.text.x = element_text(angle = 270, hjust = 0, vjust = .5, size = 8),
+        plot.title = element_text(lineheight = .8, size = 20)) +
+  facet_wrap(~species, as.table=TRUE, ncol = 3)
+
+## Close image port
+graphics.off()
+
+
+################################################################################
+### Plotting - Mean Decrease Gini - by species #################################
+################################################################################
+
+## Define output image | open image port
+png("images/lvl0600_prevalence_byspecies_MDG.png", 
+    width = 1024 * 6, 
+    height = 1024 * 6, 
+    units = "px", 
+    res = 600)
+
+ggplot(data=df.varimp.MDG.melt,
+       aes(x=variable, y=value, group=species)) +
+  geom_line() +
+  ylim(0,5) +
+  xlab("MODIS bands") +
+  ylab("Mean Decrease Gini") +
+  theme_bw() +
+  ggtitle("Prevalence - RandomForest - Mean Decrease Gini - by species") +
+  theme(axis.text.x = element_text(angle = 270, hjust = 0, vjust = .5, size = 8),
+        plot.title = element_text(lineheight = .8, size = 20)) +
+  facet_wrap(~species, as.table=TRUE, ncol = 3)
 
 ## Close image port
 graphics.off()

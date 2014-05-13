526e0ed6f297e8cdf3b618378e3371eff72cb520
schmingo <sschlauss@gmail.com>
2014-05-13 17:13:03 +0200

    minor changes

--- random_forest/lvl0550_rf_classification_prevalence_cut_singlespecies.R
+++ random_forest/lvl0550_rf_classification_prevalence_cut_singlespecies.R
@@ -72,9 +72,8 @@ df.spec.greyval <- cbind(df.greyval, tmp.species)
 
 
 ## Define Random Forest input data #############################################
+
 df.input.rf <- df.spec.greyval ## Insert input dataset here!
-
-
 train.data <- df.input.rf
 
 
@@ -102,9 +101,6 @@ rf.plot <- plot(train.rf, type="l", main = paste0("Prevalence ", s,
 
 rf.plot
 
-## Close image port
-# graphics.off()
-
 print(train.rf)
 
 print("Observed classes")
@@ -118,30 +114,5 @@ table(tmp.species)
 varimp <- importance(train.rf)
 
 
-## Define output image | open image port
-# png(paste0("images/rf_", s, "_classification_prevalence_varImp.png"), 
-#     width = 1024 * 6, 
-#     height = 748 * 6, 
-#     units = "px", 
-#     res = 600)
-
 varimp.plot <- varImpPlot(train.rf, sort = TRUE, n.var = 30,
                           main = paste0(s," - Variable importance"))
-## Close image port
-# graphics.off()
-
-
-## The first measure is computed from permuting OOB data: For each tree, 
-## the prediction error on the out-of-bag portion of the data is recorded 
-## (error rate for classification, MSE for regression). Then the same is 
-## done after permuting each predictor variable. The difference between 
-## the two are then averaged over all trees, and normalized by 
-## the standard deviation of the differences. If the standard deviation 
-## of the differences is equal to 0 for a variable, the division is 
-## not done (but the average is almost always equal to 0 in that case).
-## 
-## The second measure is the total decrease in node impurities from 
-## splitting on the variable, averaged over all trees. 
-## For classification, the node impurity is measured by the Gini index. 
-## For regression, it is measured by residual sum of squares.
-

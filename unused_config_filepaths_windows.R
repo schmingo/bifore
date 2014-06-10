9bb0f4bb40ea673b1717f28357fd9d8cd9500896
schmingo <sschlauss@gmail.com>
2014-06-10 10:33:44 +0200

    not in use anymore add implementation example

--- unused_config_filepaths_windows.R
+++ unused_config_filepaths_windows.R
@@ -18,3 +18,9 @@ path.csv <- "D:/Dropbox/Code/bifore/src/csv/kili/"
 file.varimp.MDA <- paste0(path.csv,"lvl0600_rf_prevalence_species-cut_mean100_MDA.csv")
 file.varimp.MDG <- paste0(path.csv,"lvl0600_rf_prevalence_species-cut_mean100_MDG.csv")
 
+
+
+## implementation example
+# switch(Sys.info()[["sysname"]], 
+#        "Windows" = source("D:/Code/bifore/config_filepaths_windows.R", echo=TRUE),
+#        "Linux" = source("D:/Code/bifore/config_filepaths_linux.R", echo=TRUE))

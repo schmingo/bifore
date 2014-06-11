4fad5cde83749230712858a7e7d3e07dcb8da204
schmingo <sschlauss@gmail.com>
2014-06-11 12:00:52 +0200

    update header (add license and description)

--- preprocessing/lvl0310_create_bandname_table.R
+++ preprocessing/lvl0310_create_bandname_table.R
@@ -1,14 +1,34 @@
 cat("\014")
 ################################################################################
-## BiFoRe Scripts                                                             ##
-##                                                                            ##
-## CREATE BANDNAMES DATAFRAME FOR PLOTTING ETC.                               ##
-##                                                                            ##
-##                                                                            ##
-## Author: Simon Schlauss (sschlauss@gmail.com)                               ##
-## Version: 2014-03-03                                                        ##
-##                                                                            ##
+##  
+##  BiFoRe Scripts
+##    
+##  Creates a table with MODIS bandnames.
+##  
+##  Version: 2014-03-03
+##  
 ################################################################################
+##
+##  Copyright (C) 2014 Simon Schlauss (sschlauss@gmail.com)
+##
+##
+##  This file is part of BiFoRe.
+##  
+##  BiFoRe is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##  
+##  BiFoRe is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##  
+##  You should have received a copy of the GNU General Public License
+##  along with BiFoRe.  If not, see <http://www.gnu.org/licenses/>.
+##  
+################################################################################
+
 
 ## Clear workspace
 rm(list = ls(all = TRUE))

07477fd46f429c59217b06177889b6a8b9e866bb
schmingo <sschlauss@gmail.com>
2014-07-05 12:46:04 +0200

    better comments for variableImportance

--- random_forest/lvl0400_v2_rf_classification_prevalence.R
+++ random_forest/lvl0400_v2_rf_classification_prevalence.R
@@ -57,7 +57,7 @@ setwd("D:/")
 ncores <- detectCores()-1
 
 ## Set number of RandomForest runs
-rf.runs <- 10
+rf.runs <- 5
 
 ## Runtime calculation
 starttime <- Sys.time()
@@ -269,66 +269,66 @@ for (i in seq(1:rf.runs)) {
       train.rf$confusion[2,2], #  Confusion Matrix Observed 1, Predicted 1
       train.rf$confusion[1,3], #  Classification Error 0
       train.rf$confusion[2,3], #  Classification Error 1
-      variableImportance[1,3], #  MeanDecreaseAccuracy band 01
-      variableImportance[2,3], #  MeanDecreaseAccuracy band 02
-      variableImportance[3,3], #  MeanDecreaseAccuracy band 03
-      variableImportance[4,3], #  MeanDecreaseAccuracy band 04
-      variableImportance[5,3], #  MeanDecreaseAccuracy band 05
-      variableImportance[6,3], #  MeanDecreaseAccuracy band 06
-      variableImportance[7,3], #  MeanDecreaseAccuracy band 07
-      variableImportance[8,3], #  MeanDecreaseAccuracy band 08
-      variableImportance[9,3], #  MeanDecreaseAccuracy band 09
-      variableImportance[10,3], #  MeanDecreaseAccuracy band 10
-      variableImportance[11,3], #  MeanDecreaseAccuracy band 17
-      variableImportance[12,3], #  MeanDecreaseAccuracy band 18
-      variableImportance[13,3], #  MeanDecreaseAccuracy band 19
-      variableImportance[14,3], #  MeanDecreaseAccuracy band 20
-      variableImportance[15,3], #  MeanDecreaseAccuracy band 21
-      variableImportance[16,3], #  MeanDecreaseAccuracy band 22
-      variableImportance[17,3], #  MeanDecreaseAccuracy band 23
-      variableImportance[18,3], #  MeanDecreaseAccuracy band 24
-      variableImportance[19,3], #  MeanDecreaseAccuracy band 25
-      variableImportance[20,3], #  MeanDecreaseAccuracy band 26
-      variableImportance[21,3], #  MeanDecreaseAccuracy band 27
-      variableImportance[22,3], #  MeanDecreaseAccuracy band 28
-      variableImportance[23,3], #  MeanDecreaseAccuracy band 29
-      variableImportance[24,3], #  MeanDecreaseAccuracy band 30
-      variableImportance[25,3], #  MeanDecreaseAccuracy band 31
-      variableImportance[26,3], #  MeanDecreaseAccuracy band 32
-      variableImportance[27,3], #  MeanDecreaseAccuracy band 33
-      variableImportance[28,3], #  MeanDecreaseAccuracy band 34
-      variableImportance[29,3], #  MeanDecreaseAccuracy band 35
-      variableImportance[30,3], #  MeanDecreaseAccuracy band 36
-      variableImportance[1,4], #  MeanDecreaseGini band 01
-      variableImportance[2,4], #  MeanDecreaseGini band 02
-      variableImportance[3,4], #  MeanDecreaseGini band 03
-      variableImportance[4,4], #  MeanDecreaseGini band 04
-      variableImportance[5,4], #  MeanDecreaseGini band 05
-      variableImportance[6,4], #  MeanDecreaseGini band 06
-      variableImportance[7,4], #  MeanDecreaseGini band 07
-      variableImportance[8,4], #  MeanDecreaseGini band 08
-      variableImportance[9,4], #  MeanDecreaseGini band 09
-      variableImportance[10,4], #  MeanDecreaseGini band 10
-      variableImportance[11,4], #  MeanDecreaseGini band 17
-      variableImportance[12,4], #  MeanDecreaseGini band 18
-      variableImportance[13,4], #  MeanDecreaseGini band 19
-      variableImportance[14,4], #  MeanDecreaseGini band 20
-      variableImportance[15,4], #  MeanDecreaseGini band 21
-      variableImportance[16,4], #  MeanDecreaseGini band 22
-      variableImportance[17,4], #  MeanDecreaseGini band 23
-      variableImportance[18,4], #  MeanDecreaseGini band 24
-      variableImportance[19,4], #  MeanDecreaseGini band 25
-      variableImportance[20,4], #  MeanDecreaseGini band 26
-      variableImportance[21,4], #  MeanDecreaseGini band 27
-      variableImportance[22,4], #  MeanDecreaseGini band 28
-      variableImportance[23,4], #  MeanDecreaseGini band 29
-      variableImportance[24,4], #  MeanDecreaseGini band 30
-      variableImportance[25,4], #  MeanDecreaseGini band 31
-      variableImportance[26,4], #  MeanDecreaseGini band 32
-      variableImportance[27,4], #  MeanDecreaseGini band 33
-      variableImportance[28,4], #  MeanDecreaseGini band 34
-      variableImportance[29,4], #  MeanDecreaseGini band 35
-      variableImportance[30,4])) #  MeanDecreaseGini band 36
+      variableImportance[1,3], #  MeanDecreaseAccuracy greyvalue MODIS band 01
+      variableImportance[2,3], #  MeanDecreaseAccuracy greyvalue MODIS band 02
+      variableImportance[3,3], #  MeanDecreaseAccuracy greyvalue MODIS band 03
+      variableImportance[4,3], #  MeanDecreaseAccuracy greyvalue MODIS band 04
+      variableImportance[5,3], #  MeanDecreaseAccuracy greyvalue MODIS band 05
+      variableImportance[6,3], #  MeanDecreaseAccuracy greyvalue MODIS band 06
+      variableImportance[7,3], #  MeanDecreaseAccuracy greyvalue MODIS band 07
+      variableImportance[8,3], #  MeanDecreaseAccuracy greyvalue MODIS band 08
+      variableImportance[9,3], #  MeanDecreaseAccuracy greyvalue MODIS band 09
+      variableImportance[10,3], #  MeanDecreaseAccuracy greyvalue MODIS band 10
+      variableImportance[11,3], #  MeanDecreaseAccuracy greyvalue MODIS band 17
+      variableImportance[12,3], #  MeanDecreaseAccuracy greyvalue MODIS band 18
+      variableImportance[13,3], #  MeanDecreaseAccuracy greyvalue MODIS band 19
+      variableImportance[14,3], #  MeanDecreaseAccuracy greyvalue MODIS band 20
+      variableImportance[15,3], #  MeanDecreaseAccuracy greyvalue MODIS band 21
+      variableImportance[16,3], #  MeanDecreaseAccuracy greyvalue MODIS band 22
+      variableImportance[17,3], #  MeanDecreaseAccuracy greyvalue MODIS band 23
+      variableImportance[18,3], #  MeanDecreaseAccuracy greyvalue MODIS band 24
+      variableImportance[19,3], #  MeanDecreaseAccuracy greyvalue MODIS band 25
+      variableImportance[20,3], #  MeanDecreaseAccuracy greyvalue MODIS band 26
+      variableImportance[21,3], #  MeanDecreaseAccuracy greyvalue MODIS band 27
+      variableImportance[22,3], #  MeanDecreaseAccuracy greyvalue MODIS band 28
+      variableImportance[23,3], #  MeanDecreaseAccuracy greyvalue MODIS band 29
+      variableImportance[24,3], #  MeanDecreaseAccuracy greyvalue MODIS band 30
+      variableImportance[25,3], #  MeanDecreaseAccuracy greyvalue MODIS band 31
+      variableImportance[26,3], #  MeanDecreaseAccuracy greyvalue MODIS band 32
+      variableImportance[27,3], #  MeanDecreaseAccuracy greyvalue MODIS band 33
+      variableImportance[28,3], #  MeanDecreaseAccuracy greyvalue MODIS band 34
+      variableImportance[29,3], #  MeanDecreaseAccuracy greyvalue MODIS band 35
+      variableImportance[30,3], #  MeanDecreaseAccuracy greyvalue MODIS band 36
+      variableImportance[1,4], #  MeanDecreaseGini greyvalue MODIS band 01
+      variableImportance[2,4], #  MeanDecreaseGini greyvalue MODIS band 02
+      variableImportance[3,4], #  MeanDecreaseGini greyvalue MODIS band 03
+      variableImportance[4,4], #  MeanDecreaseGini greyvalue MODIS band 04
+      variableImportance[5,4], #  MeanDecreaseGini greyvalue MODIS band 05
+      variableImportance[6,4], #  MeanDecreaseGini greyvalue MODIS band 06
+      variableImportance[7,4], #  MeanDecreaseGini greyvalue MODIS band 07
+      variableImportance[8,4], #  MeanDecreaseGini greyvalue MODIS band 08
+      variableImportance[9,4], #  MeanDecreaseGini greyvalue MODIS band 09
+      variableImportance[10,4], #  MeanDecreaseGini greyvalue MODIS band 10
+      variableImportance[11,4], #  MeanDecreaseGini greyvalue MODIS band 17
+      variableImportance[12,4], #  MeanDecreaseGini greyvalue MODIS band 18
+      variableImportance[13,4], #  MeanDecreaseGini greyvalue MODIS band 19
+      variableImportance[14,4], #  MeanDecreaseGini greyvalue MODIS band 20
+      variableImportance[15,4], #  MeanDecreaseGini greyvalue MODIS band 21
+      variableImportance[16,4], #  MeanDecreaseGini greyvalue MODIS band 22
+      variableImportance[17,4], #  MeanDecreaseGini greyvalue MODIS band 23
+      variableImportance[18,4], #  MeanDecreaseGini greyvalue MODIS band 24
+      variableImportance[19,4], #  MeanDecreaseGini greyvalue MODIS band 25
+      variableImportance[20,4], #  MeanDecreaseGini greyvalue MODIS band 26
+      variableImportance[21,4], #  MeanDecreaseGini greyvalue MODIS band 27
+      variableImportance[22,4], #  MeanDecreaseGini greyvalue MODIS band 28
+      variableImportance[23,4], #  MeanDecreaseGini greyvalue MODIS band 29
+      variableImportance[24,4], #  MeanDecreaseGini greyvalue MODIS band 30
+      variableImportance[25,4], #  MeanDecreaseGini greyvalue MODIS band 31
+      variableImportance[26,4], #  MeanDecreaseGini greyvalue MODIS band 32
+      variableImportance[27,4], #  MeanDecreaseGini greyvalue MODIS band 33
+      variableImportance[28,4], #  MeanDecreaseGini greyvalue MODIS band 34
+      variableImportance[29,4], #  MeanDecreaseGini greyvalue MODIS band 35
+      variableImportance[30,4])) #  MeanDecreaseGini greyvalue MODIS band 36
     
     ## Set names for single species dataframe
     names(df.rf.singlespecies) <- s
@@ -412,13 +412,8 @@ for (i in seq(1:rf.runs)) {
                             df.rf.allspecies[, 2:ncol(df.rf.allspecies)-1])
   names(df.rf.allspecies)[1] <- paste0("rf_", i)
   
-  #   write.csv(df.filename.csv, header = FALSE, append = TRUE)
-  #   Jeder durchgang wird hinten angefügt. Header kann nicht gesetzt werden,
-  #   es muss zusätzlich eine spalte mit i (index des loops) hinzugefügt werden.
-  
   write.table(df.rf.allspecies,
             file = paste0(path.testing, "lvl_0400_df.csv"),
-#             header = FALSE,
             append = TRUE,
             quote = FALSE,
             col.names = TRUE,
@@ -426,6 +421,7 @@ for (i in seq(1:rf.runs)) {
             dec = ",",
             sep = ";",
             )
+  
   ### Write testing dataframe ##################################################
 #   cat("\n\nWRITE TESTING DATAFRAME ", i, "\n")
 #   write.table(df.rf.allspecies,

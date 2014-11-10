cf81d629cc7bfac15ac2bed11f214ad1a588b97d
schmingo <sschlauss@gmail.com>
2014-11-10 17:37:58 +0100

    init

---
+++ plotting/trim_png.R
@@ -0,0 +1,57 @@
+cat("\014")
+################################################################################
+##  
+##  BiFoRe Scripts
+##
+##  List and prepare *.png files
+##  
+##  Version: 2014-11-10
+##  
+################################################################################
+##
+##  Copyright (C) 2014 Simon Schlauss (sschlauss@gmail.com)
+##
+##
+##  This file is part of BiFoRe.
+##  
+##  BiFoRe is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##  
+##  BiFoRe is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##  
+##  You should have received a copy of the GNU General Public License
+##  along with BiFoRe.  If not, see <http://www.gnu.org/licenses/>.
+##  
+################################################################################
+
+
+## Clear workspace
+rm(list = ls(all = TRUE))
+
+## Required libraries
+lib <- c("ggplot2", "reshape2")
+lapply(lib, function(...) require(..., character.only = TRUE))
+
+## Set working directory
+# setwd("/home/schmingo/Dropbox/Code/bifore/src/")
+setwd("D:/Dropbox/Code/bifore/src/")
+
+## Set output filepath
+path.images <- "images/raw/"
+
+
+
+################################################################################
+### List *.png files ###########################################################
+################################################################################
+
+fls_png <- list.files(path.images, pattern = ".png", full.names = TRUE, recursive = TRUE)
+fls_png
+
+for (i in fls_png)
+  system(paste("convert -trim", i, i))

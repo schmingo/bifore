6520c6199fa9a04f281cf590941f4a421beae167
schmingo <sschlauss@gmail.com>
2014-12-01 19:52:00 +0100

    Update Orthoptera observation plots - update used dataset - remove unused second dataset - update legend header

---
+++ plotting/plot_lvl0100_orthoptera_observations_map.R
@@ -0,0 +1,164 @@
+cat("\014")
+################################################################################
+##  
+##  BiFoRe Scripts
+##
+##  Plot Orthoptera observations at Mt. Kilimanjaro
+##  
+##  Version: 2014-03-16
+##  
+################################################################################
+##
+##  Copyright (C) 2014 Simon Schlauss (sschlauss@gmail.com)
+##
+##
+##  This file is part of BiFoRe.
+##  
+##  BiFoRe is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##  
+##  BiFoRe is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##  
+##  You should have received a copy of the GNU General Public License
+##  along with BiFoRe.  If not, see <http://www.gnu.org/licenses/>.
+##  
+################################################################################
+
+
+## Clear workspace
+rm(list = ls(all = TRUE))
+
+## Required libraries
+lib <- c("ggplot2", "ggmap", "RJSONIO")
+lapply(lib, function(...) require(..., character.only = TRUE))
+
+## Set working directory
+# setwd("/home/schmingo/Dropbox/Code/bifore/src/")
+setwd("D:/Dropbox/Code/bifore/src/")
+
+
+################################################################################
+### Import data ################################################################
+################################################################################
+
+data.raw <- read.csv2("csv/kili/lvl0100_biodiversity_data.csv",
+                     dec = ",",
+                     header = TRUE, 
+                     stringsAsFactors = TRUE,
+                     )
+
+## Read date column as a date
+data.raw$date_observation <- as.Date(data.raw$date_observation, format="%Y-%j")
+
+data.sp <- data.raw
+
+
+################################################################################
+### Subsetting #################################################################
+################################################################################
+
+data.observations <- data.raw[,1:13]
+
+## Create additional year column
+data.observations$year <- as.numeric(format(data.observations$date_observation, "%Y"))
+
+
+################################################################################
+### Plot all orthoptera observations ###########################################
+################################################################################
+
+## Define output image | open image port
+png("images/lvl0100_map_orthoptera_observations.png", 
+    width = 1024 * 6, 
+    height = 748 * 6, 
+    units = "px", 
+    res = 600)
+
+kili.extent <- get_map(location = c(36.93865,
+                                    -3.454621,
+                                    37.76235,
+                                    -2.775392),
+                       scale = "auto",
+                       maptype = "satellite",
+                       color = "bw",
+                       source = "google")
+
+kilimap <- ggmap(kili.extent, 
+                 extent = "normal",
+                 maprange = TRUE)
+
+orthoptera.obs1 <- geom_point(aes(x = lon,
+                                  y = lat,
+                                  size = 1,
+                                  colour = nr.of.species),
+                              show_guide = FALSE,
+                              data = data.observations)
+
+colourscale <- scale_colour_gradient(low = "white", 
+                                     high = "darkgreen", 
+                                     name = "no. of species",
+                                     limits=c(0, 30))
+
+style.plot1 <- theme(legend.background = element_rect(colour = "black"),
+               plot.title = element_text(size = 20))
+
+labeling.plot1 <- labs(title = "Orthoptera observations 2002 - 2012")
+
+kilimap + orthoptera.obs1 + colourscale + labeling.plot1 + style.plot1
+
+
+## Close image port
+graphics.off()
+
+
+################################################################################
+### Plot Orthoptera Observations per year ######################################
+################################################################################
+
+## Define output image | open image port
+png("images/lvl0100_map_orthoptera_observations_year.png", 
+    width = 1024 * 6, 
+    height = 748 * 6, 
+    units = "px", 
+    res = 600)
+
+
+kili.extent <- get_map(location = c(36.93865,
+                                    -3.454621,
+                                    37.76235,
+                                    -2.775392),
+                       scale = "auto",
+                       maptype = "satellite",
+                       color = "bw",
+                       source = "google")
+
+kilimap <- ggmap(kili.extent, 
+                 extent = "normal",
+                 maprange = TRUE)
+
+orthoptera.obs1 <- geom_point(aes(x = lon,
+                                  y = lat,
+                                  colour = nr.of.species),
+                              show_guide = FALSE,
+                              data = data.observations)
+
+colourscale <- scale_colour_gradient(low = "white", 
+                                     high = "darkgreen", 
+                                     name = "no. of species",
+                                     limits=c(0, 30))
+
+style.plot2 <- theme(legend.background = element_rect(colour = "black"),
+                     plot.title = element_text(size = 20))
+
+labeling.plot2 <- labs(title = "Orthoptera observations 2002 - 2012")
+
+kilimap + orthoptera.obs1 + colourscale + facet_wrap(~ year) + style.plot2 + labeling.plot2
+
+
+# ## Close image port
+graphics.off()
